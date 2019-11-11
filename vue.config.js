65d1be678eeae4c886e8710e255fdaf04f9e8bad
bing.sun@seaboxdata.com <441286116ABc>
2019-11-11 09:33:53 +0800

    全局配置

--- vue.config.js
+++ vue.config.js
@@ -33,6 +33,13 @@ let product = {
     filename: 'index.html',
     title: '云商平台-云函数',
     chunks: ['chunk-vendors', 'chunk-common', 'CM']
+  },
+  CAM: {
+    entry: 'src/views/CAM/main.js', // page 的入口
+    template: 'src/public/index.html', // 模板来源
+    filename: 'index.html', // 在 dist/index.html 的输出
+    title: '云商平台-云服务器', // 当使用 title 选项时，template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+    chunks: ['chunk-vendors', 'chunk-common', 'CAM'] // 在这个页面中包含的块，默认情况下会包含,提取出来的通用 chunk 和 vendor chunk。
   }
 }
 let page = {}
