2a7d8c2f73f391ec3d76b6ab6494df4811355e07
wangmingyang <wmy_near@163.com>
2019-11-15 19:00:29 +0800

    管理实例

--- src/views/CCN/ccnPage/module/tabFour.vue
+++ src/views/CCN/ccnPage/module/tabFour.vue
@@ -4,26 +4,34 @@
     <div class="table">
       <el-table :data="tableData" style="width: 100%">
         <template slot="empty">{{$t('CCN.CCN.tabs.tab1no')}}</template>
-        <el-table-column prop="name" :label="$t('CCN.CCN.tabs.tab4tr1')" width> </el-table-column>
-        <el-table-column prop="" :label="$t('CCN.CCN.tabs.tab4tr2') " width>
+        <el-table-column prop="DestinationCidrBlock" :label="$t('CCN.CCN.tabs.tab4tr1')" width></el-table-column>
+        <el-table-column prop="Enabled" :label="$t('CCN.CCN.tabs.tab4tr2') " width>
           <template slot-scope="scope">
-            <div v-if="scope.row.status == 0" class="close_color">关闭</div>
-            <div v-if="scope.row.status == 1" class="off_color">有效</div>
+            <div v-if="scope.row.Enabled == true" class="off_color">有效</div>
+            <!-- <div v-if="scope.row.Enabled == 0" class="close_color">关闭</div> -->
+            <div v-else>关闭</div>
           </template>
         </el-table-column>
-        <el-table-column prop="" :label="$t('CCN.CCN.tabs.tab4tr3')" width>
+        <el-table-column prop="InstanceId" :label="$t('CCN.CCN.tabs.tab4tr3')" width>
           <template slot-scope="scope">
-            <el-button @click="handleClick(scope.row)" type="text">{{
-              scope.row.vpcName
-            }}</el-button>
-            <p class="edit">12121</p>
+            <el-button @click="handleClick(scope.row)" type="text">
+              {{ scope.row.InstanceId }}
+            </el-button>
+            <p class="edit">{{ scope.row.InstanceName }}</p>
           </template>
         </el-table-column>
-        <el-table-column prop="nextRegion" :label="$t('CCN.CCN.tabs.tab4tr4')"> </el-table-column>
-        <el-table-column prop="updateTime" :label="$t('CCN.CCN.tabs.tab4tr5')" width></el-table-column>
+        <el-table-column prop="InstanceRegion" :label="$t('CCN.CCN.tabs.tab4tr4')"></el-table-column>
+        <el-table-column prop="UpdateTime" :label="$t('CCN.CCN.tabs.tab4tr5')" width></el-table-column>
         <el-table-column prop="operate" :label="$t('CCN.CCN.tabs.tab4tr6')" width>
           <template slot-scope="scope">
-            <el-switch v-model="value1"> </el-switch>
+            <!-- <el-switch v-model="value1"></el-switch> -->
+            <el-switch
+              style="display: block;"
+              v-model="scope.row.Enabled"
+              @change="change(scope.row)"
+              active-color="#13ce66"
+              inactive-color="#ff4949">
+            </el-switch>
           </template>
         </el-table-column>
       </el-table>
@@ -35,16 +43,9 @@
 export default {
   data () {
     return {
+      visible2: false,
       value1: true,
-      tableData: [
-        {
-          name: '10.0.0.0/24',
-          vpcName: 'vpc-kl9iqcz0',
-          status: '1',
-          nextRegion: '中国台北',
-          updateTime: '2019-11-07 14：05：12'
-        }
-      ] // 列表数据
+      tableData: [{}] // 列表数据
     }
   },
   created () {
@@ -62,7 +63,26 @@ export default {
       // 查询-路由表
       this.$axios.post("vpc2/DescribeCcnRoutes", params).then(res => {
         console.log(res);
+        this.tableData = res.Response.RouteSet;
       })
+    },
+    change: function (route) {
+      var params = {
+        Version: "2017-03-12",
+        Region: "ap-taipei",
+        CcnId: this.ccnId,
+        'RouteIds.0': route.RouteId
+      }
+      console.log(route)
+      if (route.Enabled) {  // true启用
+        this.$axios.post("vpc2/EnableCcnRoutes", params).then(res => {
+          console.log(res);
+        })
+      } else {  // false 禁用
+        this.$axios.post("vpc2/DisableCcnRoutes", params).then(res => {
+          console.log(res);
+        })
+      }
     }
   }
 }
