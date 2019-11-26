a413005bbff3ba21fecfe890abf49348ac96ac33
[shimengyu] <[962089280@qq.com]>
2019-11-26 10:59:22 +0800

    cap

--- src/language/ft.js
+++ src/language/ft.js
@@ -5,5 +5,6 @@ module.exports = {
   'SCF': require('@/views/SCF/language/ft'),
   'CAM': require('@/views/CAM/language/ft'),
   'DDOS': require('@/views/DDOS/language/ft'),
-  'CLA': require('@/views/CLA/language/ft')
+  'CLA': require('@/views/CLA/language/ft'),
+  'CAP': require('@/views/CAP/language/ft')
 }
--- src/language/jt.js
+++ src/language/jt.js
@@ -4,5 +4,6 @@ module.exports = {
   'CCN': require('@/views/CCN/language/jt'),
   'SCF': require('@/views/SCF/language/jt'),
   'DDOS': require('@/views/DDOS/language/jt'),
-  'CLA': require('@/views/CLA/language/jt')
+  'CLA': require('@/views/CLA/language/jt'),
+  'CAP': require('@/views/CAP/language/jt')
 }
