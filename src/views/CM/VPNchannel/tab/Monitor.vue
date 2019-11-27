3aa86b7374d6b0816c58d2189a2e078d0eccbd05
13698810327@163.com <13698810327@163.com>
2019-11-27 15:49:32 +0800

    VPN通道

--- src/views/CM/VPNchannel/tab/Monitor.vue
+++ src/views/CM/VPNchannel/tab/Monitor.vue
@@ -2,9 +2,7 @@
   <div class='Monitor'>
 
     <!-- 时间粒度搜素 -->
-    <XTimeX v-on:qiehuan="GetDat"
-      :classsvalue='value'></XTimeX>
-
+    <XTimeX v-on:switchData="GetDat" :classsvalue='value'></XTimeX>
     <div class='box-dis p-style'>
       <p><i class="el-icon-info"></i>注释：Max、Min和Avg数值统计为当前折线图内所有点的最大值、最小值和平均值</p>
       <p>
@@ -13,38 +11,26 @@
     </div>
     <div class='box-table'>
       <!-- 表格 -->
-      <el-table :data="tableData"
-        style="width: 100%">
-
-        <el-table-column prop="">
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column prop="" width="130">
           <template slot-scope="scope">
-            <!-- <span style="font-size:12px;font-weight:bolder;">
-            {{scope.row.Name}}
-              <el-popover
-                placement="bottom-start"
-                title=""
-                width="200"
-                trigger="hover">
-                <p>{{scope.row.Name}}</p>
+            <span style="font-size:12px;font-weight:bolder;">
+              {{scope.row.MetricName | UpName(value)}}
+              <el-popover placement="bottom-start" title="" width="200" trigger="hover">
+                <p>{{scope.row.MetricName | UpTitle(value)}}</p>
                 <i class="el-icon-warning" slot="reference"></i>
               </el-popover>
-            </span> -->
-            <p>CPU利用率</p>
+            </span>
+
           </template>
         </el-table-column>
 
-        <el-table-column prop="Y"
-          width="600">
+        <el-table-column prop="DataPoints" width="550">
           <template slot-scope="scope">
-            <div class='echart'>
-              <echart-line id="diskEchearrts-line"
-                :time='timeData'
-                :opData='scope.row.dataPoints'
-                :unit='diskUnit'
-                :title="diskTitle"
-                :period=period
-                :scale=3
-                :xdata=false>
+            <p v-if="scope.row.DataPoints[0].Values.length==0">暂无数据</p>
+            <div class='echart' v-if="scope.row.DataPoints[0].Values.length!=0">
+              <echart-line id="diskEchearrts-line" :time='scope.row.DataPoints[0].Timestamps | UpTime'
+                :opData='scope.row.DataPoints[0].Values' :scale=3 :period=period :xdata=false>
               </echart-line>
             </div>
 
@@ -53,53 +39,42 @@
 
         <el-table-column prop="">
           <template slot-scope="scope">
-            <p>{{scope.row.max}}</p>
-            <p>{{scope.row.max1}}</p>
+            <p>Max:</p>
+            <p>--</p>
 
           </template>
         </el-table-column>
 
         <el-table-column prop="">
           <template slot-scope="scope">
-            <p>{{scope.row.min}}</p>
-            <p>{{scope.row.min1}}</p>
+            <p>Min:</p>
+            <p>--</p>
 
           </template>
         </el-table-column>
 
         <el-table-column prop="">
           <template slot-scope="scope">
-            <p>{{scope.row.avg}}</p>
-            <p>{{scope.row.avg1}}</p>
+            <p>Avg:</p>
+            <p>--</p>
 
           </template>
         </el-table-column>
 
         <el-table-column prop="">
           <template slot-scope="scope">
-            <i class="el-icon-menu i-font"
-              @click="Modality(scope.row.dataPoints)"></i>
-            <i class="el-icon-download i-font"></i>
+            <p> <i class="el-icon-menu i-font" style="font-size:26px;" @click="Modality(scope.row.MetricName)"></i>
+            </p>
 
           </template>
         </el-table-column>
 
       </el-table>
       <!-- 模态框 -->
-      <el-dialog title="集群健康状态"
-        :visible.sync="dialogVisible"
-        width="60%"
-        :before-close="handleClose">
-        <XTimeX v-on:qiehuan="GetDat"
-          :classsvalue='value'></XTimeX>
-        <echart-line id="diskEchearrts-line"
-          class="echart-wh"
-          :time='timeData'
-          :opData='opDiskData'
-          :unit='diskUnit'
-          :period=period
-          :title="diskTitle"
-          :xdata=true>
+      <el-dialog title="集群健康状态" :visible.sync="dialogVisible" width="60%" :before-close="handleClose">
+        <XTimeX v-on:switchData="GetDat" :classsvalue='value'></XTimeX>
+        <echart-line id="diskEchearrts-line" class="echart-wh" :time='timeData | UpTime' :opData='jingData'
+          :period=period :xdata=true>
         </echart-line>
       </el-dialog>
 
@@ -109,165 +84,272 @@
 </template>
 
 <script>
-import axios from 'axios';
-import XTimeX from '@/components/TimeX';
-import echartLine from '@/components/echars-line';
-import { All_MONITOR } from '@/constants';
-export default {
-  data() {
-    return {
-      ID: this.$route.query.id,
-      period: '',
-      Start_End: [],
-      value: 1,
-      dialogVisible: false, // 模态框 （放大后的折线图）
-      pageIndex: 1, // 当前页
-      pageSize: 10, // 每页数
-      totalPage: 0, // 表格数据数组长度
-      tableData: [], // 获取列表数据
-      timeData: [], // 折线图的x轴数据
-      jingData: [],
-    };
-  },
-  components: {
-    echartLine,
-    XTimeX,
-  },
-  created() {},
-  methods: {
-    GetDat(data) {
-      this.period = data[0];
-      this.timeData = data[1];
-      this.Start_End = data[2];
-      this.value = data[3];
-      const metricNArr = [
-        'cpu_usage',
-        'cpu_loadavg',
-        'mem_used',
-        'mem_usage',
-        'tcp_curr_estab',
-      ];
-      this.tableData = [];
-      for (let i = 0; i < metricNArr.length; i++) {
-        this.Obtain(metricNArr[i]);
+  import moment from 'moment';
+  import XTimeX from '@/components/public/TimeXK';
+  import echartLine from '@/components/public/echars-line';
+  import {
+    All_MONITOR
+  } from '@/constants';
+  export default {
+    data() {
+      return {
+        ID: this.$route.query.id,
+        period: '',
+        Start_End: [],
+        value: 1,
+        dialogVisible: false, // 模态框 （放大后的折线图）
+        pageIndex: 1, // 当前页
+        pageSize: 10, // 每页数
+        totalPage: 0, // 表格数据数组长度
+        tableData: [], // 获取列表数据
+        timeData: [], // 折线图的x轴数据
+        jingData: [],
+        MetricName: ''
+      };
+    },
+    components: {
+      echartLine,
+      XTimeX,
+    },
+    created() {},
+    methods: {
+      GetDat(data) {
+        this.period = data[0];
+        this.Start_End = data[1];
+        this.value = data[2]
+        const metricNArr = [
+          'Outbandwidth',
+          'Inbandwidth',
+          'Outpkg',
+          'Inpkg',
+          'Pkgdrop',
+          'Delay'
+        ];
+        this.tableData = []
+        for (let i = 0; i < metricNArr.length; i++) {
+          this.Obtain(metricNArr[i]);
+        }
+        if (this.MetricName) {
+          this.getModality(this.MetricName)
+        }
+      },
+      // 
+      Obtain(metricN) {
+        const param = {
+          Version: '2018-07-24',
+          Region: this.$cookie.get('regionv2'),
+          Namespace: 'QCE/VPNX',
+          MetricName: metricN,
+          'Instances.0.Dimensions.0.Name': 'vpnConnId',
+          'Instances.0.Dimensions.0.Value': this.ID,
+          Period: this.period,
+          StartTime: this.Start_End.StartTIme,
+          EndTime: this.Start_End.EndTIme,
+        };
+        this.axios.post(All_MONITOR, param).then((data) => {
+          this.tableData.push(data.Response);
+        });
+      },
+      getModality(MetricName) {
+        const param = {
+          Version: '2018-07-24',
+          Region: this.$cookie.get('regionv2'),
+          Namespace: 'QCE/VPNX',
+          MetricName: MetricName,
+          'Instances.0.Dimensions.0.Name': 'vpnConnId',
+          'Instances.0.Dimensions.0.Value': this.ID,
+          Period: this.period,
+          StartTime: this.Start_End.StartTIme,
+          EndTime: this.Start_End.EndTIme,
+        };
+        this.axios.post(All_MONITOR, param).then((data) => {
+          this.timeData = data.Response.DataPoints[0].Timestamps
+          this.jingData = data.Response.DataPoints[0].Values
+        });
+      },
+      // 模态框
+      Modality(MetricName) {
+        this.MetricName = MetricName
+        this.dialogVisible = true;
+        this.getModality(this.MetricName)
+      },
+      handleClose(done) {
+        done();
+      },
+
+    },
+    filters: {
+      UpName(value) {
+        if (value === 'Outbandwidth') {
+          return (value = '外网出带宽');
+        }
+        if (value === 'Inbandwidth') {
+          return (value = '外网入带宽');
+        }
+        if (value === 'Outpkg') {
+          return (value = '出包量');
+        }
+        if (value === 'Inpkg') {
+          return (value = '入包量');
+        }
+        if (value === 'Pkgdrop') {
+          return (value = '丢包率');
+        }
+        if (value === 'Delay') {
+          return (value = '时延');
+        }
+        if (value === '') {
+          return (value = '');
+        }
+      },
+      UpTitle(value) {
+        if (value === 'Outbandwidth') {
+          return (value = 'VPN通道平均每秒出流量');
+        }
+        if (value === 'Inbandwidth') {
+          return (value = 'VPN通道平均每秒入流量');
+        }
+        if (value === 'Outpkg') {
+          return (value = 'VPN通道平均每秒出包量');
+        }
+        if (value === 'Inpkg') {
+          return (value = 'VPN通道平均每秒入包量');
+        }
+        if (value === 'Pkgdrop') {
+          return (value = 'VPN探测一分钟的丢包比例');
+        }
+        if (value === 'Delay') {
+          return (value = 'VPN探测一分钟的平均时延');
+        }
+        if (value === '') {
+          return (value = '');
+        }
+      },
+      UpTime(value) {
+        let timeArr = []
+        for (let i = 0; i < value.length; i++) {
+          let uptime = moment(value[i] * 1000).format(
+            'YYYY-MM-DD HH:mm:ss',
+          );
+          timeArr.push(uptime)
+        }
+
+        return timeArr
       }
     },
-    Obtain(metricN) {
-      const param = {
-        Region: this.$cookie.get('regionv1'),
-        namespace: 'qce/cvm',
-        metricName: metricN,
-        'dimensions.0.name': 'unInstanceId',
-        'dimensions.0.value': this.ID,
-        period: this.period,
-        startTime: this.Start_End.StartTIme,
-        endTime: this.Start_End.EndTIme,
-      };
-      this.axios.post(All_MONITOR, param).then((data) => {
-        this.tableData.push(data);
-      });
-    },
+  };
 
-    // 模态框
-    Modality(Y) {
-      this.dialogVisible = true;
-      this.opDiskData = Y;
-    },
-
-    handleClose(done) {
-      done();
-    },
-  },
-};
 </script>
 
-<style  scoped lang="scss">
-.Monitor {
-  height: 1800px;
-  background: #ffffff;
-  margin-top: 20px;
-  border: 1px solid #cccccc;
-  -webkit-box-shadow: 0px 3px 3px #c8c8c8;
-  -moz-box-shadow: 0px 3px 3px #c8c8c8;
-  box-shadow: 0px 3px 3px #c8c8c8;
-}
-.box-dis {
-  display: flex;
-  justify-content: space-between;
-  margin-top: 40px;
+<style scoped lang="scss">
+  .Monitor {
+    background: #ffffff;
+    margin-top: 20px;
+    margin-bottom: 50px;
+    border: 1px solid #cccccc;
+    -webkit-box-shadow: 0px 3px 3px #c8c8c8;
+    -moz-box-shadow: 0px 3px 3px #c8c8c8;
+    box-shadow: 0px 3px 3px #c8c8c8;
+  }
+
+  .box-dis {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 40px;
+
+    .btn-style {
+      margin-left: 20px;
+      display: flex;
+      line-height: 32px;
+
+      .drop {
+        margin-left: 30px;
+
+        span {
+          color: #cccccc;
+          font-size: 10px;
+        }
+
+        ::v-deep.el-input {
+          width: 100px !important;
+          border: none;
+        }
+      }
+    }
+
+    p:nth-child(1) {
+      color: #ccc;
+      font-size: 14px;
+      margin-left: 30px;
+    }
+
+    p:nth-child(2) {
+      margin-right: 20px;
+    }
+  }
+
+  .btn-sty {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .p-dis {
+    display: flex;
+
+    span {
+      line-height: 40px;
+    }
+
+    .width-date {
+      width: 150px;
+      margin-left: 20px;
+    }
+  }
+
+  .margin-row {
+    margin-top: 30px;
+    margin-left: 55%;
+  }
+
+  .dateheight {
+    height: 34px;
+  }
+
+  ::v-deep.echart-wh {
+    width: 100% !important;
+    height: 500px !important;
+  }
+
   .btn-style {
     margin-left: 20px;
     display: flex;
     line-height: 32px;
+
     .drop {
-      margin-left: 30px;
+      margin-left: 500px;
+
       span {
         color: #cccccc;
         font-size: 10px;
       }
-       ::v-deep.el-input {
+
+      ::v-deep.el-input {
         width: 100px !important;
         border: none;
       }
     }
   }
-  p:nth-child(1) {
-    color: #ccc;
-    font-size: 14px;
-    margin-left: 30px;
+
+  .box-table {
+    width: 100%;
   }
-  p:nth-child(2) {
-    margin-right: 20px;
+
+  ::v-deep.i-font {
+    font-size: 36px;
   }
-}
-.btn-sty {
-  display: flex;
-  justify-content: space-between;
-}
-.p-dis {
-  display: flex;
-  span {
-    line-height: 40px;
+
+  ::v-deep.el-button--small {
+    font-size: 14px !important;
   }
-  .width-date {
-    width: 150px;
-    margin-left: 20px;
-  }
-}
-.margin-row {
-  margin-top: 30px;
-  margin-left: 55%;
-}
-.dateheight {
-  height: 34px;
-}
- ::v-deep.echart-wh {
-  width: 1100px !important;
-  height: 500px !important;
-}
-.btn-style {
-  margin-left: 20px;
-  display: flex;
-  line-height: 32px;
-  .drop {
-    margin-left: 500px;
-    span {
-      color: #cccccc;
-      font-size: 10px;
-    }
-     ::v-deep.el-input {
-      width: 100px !important;
-      border: none;
-    }
-  }
-}
-.box-table {
-  width: 100%;
-}
- ::v-deep.i-font {
-  font-size: 36px;
-}
- ::v-deep.el-button--small {
-  font-size: 14px !important;
-}
+
 </style>
