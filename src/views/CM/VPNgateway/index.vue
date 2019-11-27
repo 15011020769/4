35de2173fc7b38ac0c4a2ae0d71b1ec330ccf860
13698810327@163.com <13698810327@163.com>
2019-11-27 16:07:55 +0800

    VPN网关

--- src/views/CM/VPNgateway/index.vue
+++ src/views/CM/VPNgateway/index.vue
@@ -2,176 +2,230 @@
   <div>
     <!-- 城市按钮 -->
     <div class="CVM-title">VPN网关</div>
-    <Cities :cities="cities"
-      :value.sync="selectedRegion"
-      @changeCity="changeCity"
-      class="city" />
-
-    <!-- 搜索 -->
-    <div class="Right-style">
-      <el-input placeholder="请输入IP或主机名"
-        v-model="search"
-        size='small '
-        class="input-with-select esach-inputL">
-        <el-button slot="append"
-          @click="btnsearch()"
-          icon="el-icon-search"></el-button>
-      </el-input>
+    <div class="tool">
+      <Cities
+        :cities="cities"
+        class="city"
+        :Cityvalue.sync="selectedRegion"
+        @changeCity="changeCity"
+      />
+      <!-- 搜索 -->
+      <SEARCH
+        :searchOptions="searchOptions"
+        :searchValue="searchValue"
+        @changeValue="changeValue"
+        :searchInput="searchInput"
+        @changeinput="changeinput"
+        @clicksearch="clicksearch"
+      ></SEARCH>
     </div>
     <!-- 表格 -->
     <div class="Table-SY">
-      <el-table :data="TbaleData"
+      <el-table
+        :data="ProTableData.slice((currpage - 1) * pagesize, currpage * pagesize)"
         height="550"
-        style="width: 100%">
-        <el-table-column prop=""
-          label="ID/主机名">
+        style="width: 100%"
+      >
+        <el-table-column prop label="ID/主机名 ">
           <template slot-scope="scope">
             <p>
-              <a @click="jump(scope.row.unVpnGwId)"
-                style="cursor:pointer;">{{scope.row.unVpnGwId}}</a>
+              <a @click="jump(scope.row.vpcId)" style="cursor:pointer;">{{scope.row.vpcId}}</a>
             </p>
-            <p>{{scope.row.vpnGwName}}</p>
+            {{ scope.row.vpnGwName}}
           </template>
         </el-table-column>
-
-        <el-table-column prop=""
-          label="监控">
+        <el-table-column prop label="监控">
           <template slot-scope="scope">
-            <p style="font-size:26px;">
-              <a @click="jump(scope.row.peeringConnectionId)"
-                style="cursor:pointer;">
-                <i class="iconfont icon-jiankong"></i>
-              </a>
-            </p>
-
+            <i class="el-icon-share"></i>
           </template>
         </el-table-column>
-
-        <el-table-column prop=""
-          label="状态">
+        <el-table-column prop label="状态">
           <template slot-scope="scope">
-            <p style="color: #06c290"> 暂无数据</p>
+            <p
+              :class="[0,2,4].indexOf(scope.row.vpcConnStatus) != -1 ? 'orange' : [1,3].indexOf(scope.row.vpcConnStatus) != -1 ? 'red' : 'green'"
+            >{{vpcConnState[scope.row.vpnGwStatus]}}</p>
           </template>
         </el-table-column>
 
-        <el-table-column prop=""
-          label="所属网络">
-          <template slot-scope="scope">
-            <a href="">{{scope.row.unVpcId}}</a>
-            {{scope.row.vpcName}}
-          </template>
+        <el-table-column prop label="所属网络">
         </el-table-column>
 
+        <el-table-column label="健康状态">
+        </el-table-column>
+        <el-table-column prop label="告警策略数">
+        </el-table-column>
       </el-table>
       <div class="Right-style pagstyle">
-        <el-pagination @size-change="handleSizeChange"
+        <el-pagination
+          @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
-          :current-page="pageIndex"
-          :page-sizes="[10, 20, 30, 40,50]"
+          :page-sizes="[20, 30, 40,50,100]"
           :page-size="pagesize"
           layout="total, sizes, prev, pager, next, jumper"
-          :total="totalCount">
-        </el-pagination>
+          :total="ProTableData.length"
+        ></el-pagination>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import Cities from '@/components/Cities';
-import { DISK_CITY, VPN_LIST } from '@/constants';
+import Cities from "@/components/public/CITY";
+import SEARCH from "@/components/public/SEARCH";
+import { ALL_CITY, VPN_LIST, ALL_PROJECT } from "@/constants";
 export default {
   data() {
     return {
+      searchOptions: [
+        {
+          value: "vpcId",
+          label: "ID"
+        },
+        {
+          value: "vpnGwName",
+          label: "名称"
+        }
+      ],
+      vpcConnState: {
+        0: "创建中",
+        1: "创建出错",
+        2: "修改中",
+        3: "修改出错",
+        4: "删除中",
+        5: "删除出错",
+        6: "运行正常"
+      },
+      searchValue: "",
+      instanceStatus: {
+        PENDING: "创建中",
+        LAUNCH_FAILED: "创建失败",
+        RUNNING: "运行中",
+        STOPPED: "已关机",
+        STARTING: "开机中",
+        STOPPING: "关机中",
+        REBOOTING: "重启中",
+        SHUTDOWN: "待回收",
+        TERMINATING: "销毁中"
+      },
+      RestrictState: {
+        NORMAL: "健康",
+        EXPIRED: "过期",
+        PROTECTIVELY_ISOLATED: "隔离"
+      },
       cities: [],
-      selectedRegion: '', // 默认选中城市
+      selectedRegion: "ap-taipei", // 默认选中城市
       selectedCity: {}, // 切换城市
-      search: '', // 搜索
+      search: "", // 搜索
+      searchInput: "",
       TbaleData: [], // 表格数据
-      pagesize: 10, // 分页条数
-      pageIndex: 1, // 当前页码
-      totalCount: '', // 条数
-      // 列表数据处理
+      ProjectData: [], // 项目列表数据
+      ProTableData: [], // 添加完项目列表的表格数据
+      pagesize: 20, // 分页条数
+      currpage: 1 // 当前页码
     };
   },
   created() {
     this.GetCity();
-  },
-  watch: {
-    // 监听城市变化----数据变化
-    selectedRegion() {},
+    this.GetTabularData();
   },
   components: {
     Cities,
+    SEARCH
   },
   methods: {
     // 获取城市列表
     GetCity() {
-      this.axios.get(`${DISK_CITY}?product=MONITOR`).then((data) => {
+      this.axios.get(ALL_CITY).then(data => {
         this.cities = data.data;
         this.selectedRegion = data.data[0].Region;
         this.selectedCity = data.data[0];
-        this.$cookie.set('regionv1', this.selectedCity.regionCode);
-        this.$cookie.set('regionv2', this.selectedCity.Region);
-        this.GetTabularData();
+        this.$cookie.set("regionv2", this.selectedCity.Region);
       });
     },
     // 切换城市
     changeCity(city) {
       this.selectedCity = city;
-      this.$cookie.set('regionv1', city.regionCode);
-      this.$cookie.set('regionv2', city.Region);
+      this.$cookie.set("regionv2", city.Region);
       this.GetTabularData();
     },
-    // 获取表格数据
+    //选择搜索条件
+    changeValue(val) {
+      this.searchValue = val;
+    },
+    changeinput(val) {
+      this.searchInput = val;
+      if (this.searchInput === "") {
+        this.GetTabularData();
+      }
+    },
+    clicksearch(val) {
+      this.searchInput = val;
+      if (this.searchInput !== "" && this.searchValue !== "") {
+        this.GetTabularData();
+      } else {
+        this.$message.error("请输入正确搜索信息");
+      }
+    },
+    // 添加项目列表的表格数据
     GetTabularData() {
       const param = {
         Region: this.selectedRegion,
-        offset: this.pageIndex * this.pagesize - this.pagesize,
-        limit: this.pagesize,
+        Version: "2017-03-12",
+        Offset: this.currpage * this.pagesize - this.pagesize,
+        Limit: this.pagesize
       };
-      this.axios.post(VPN_LIST, param).then((data) => {
-        this.totalCount = data.totalCount;
+      if (this.searchValue !== "" && this.searchInput !== "") {
+        param[this.searchValue] = this.searchInput;
+      }
+      const paramS = {
+        allList: 0
+      };
+      // 获取表格数据
+      this.axios.post(VPN_LIST, param).then(data => {
         this.TbaleData = data.data;
-        // console.log(this.TbaleData);
+        this.ProTableData = this.TbaleData;
       });
     },
     handleSizeChange(val) {
       this.pagesize = val;
+      this.currpage = 1;
       this.GetTabularData();
     },
     handleCurrentChange(val) {
-      this.pageIndex = val;
+      this.currpage = val;
       this.GetTabularData();
     },
     jump(id) {
       this.$router.push({
-        name: 'CMVPNdetails',
+        name: "CMVPNdetails",
         query: {
-          id,
-        },
+          id
+        }
       });
-    },
-    // 搜索按钮
-    btnsearch() {
-      const search = this.search;
-      if (search) {
-        this.TbaleData = this.TbaleData.filter(product => Object.keys(product).some(
-          key => String(product[key])
-            .toLowerCase()
-            .indexOf(search) > -1,
-        ));
-      } else {
-        this.GetTabularData();
-        this.TbaleData = this.TbaleData;
-      }
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style scoped lang="scss">
+.green {
+  color: green;
+}
+
+.red {
+  color: red;
+}
+
+.orange {
+  color: orange;
+}
+
+.tool {
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 20px 0 20px;
+}
+
 .CVM-title {
   background: #fff;
   line-height: 60px;
@@ -179,21 +233,22 @@ export default {
   padding-left: 20px;
   font-size: 16px;
 }
-.city {
-  margin: 20px 20px 0 20px;
-}
+
 .Right-style {
   display: flex;
   justify-content: flex-end;
+
   .esach-inputL {
     width: 300px;
     margin-right: 20px;
   }
 }
+
 .Table-SY {
   background: #fff;
   margin: 20px;
 }
+
 .pagstyle {
   padding: 20px;
 }
