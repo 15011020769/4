009e1d147a35a9dfdfcd6e37f613084861747256
zhaobobin <254787741@qq.com>
2019-11-27 15:05:00 +0800

    弹性公网IP

--- src/constants/CM.js
+++ src/constants/CM.js
@@ -5,18 +5,18 @@ export const ALL_PROJECT = 'account/DescribeProject' // 项目列表
 
 // // 云服务器
 export const CVM_LIST = 'cvm2/DescribeInstances' // 列表
-// Mysql
+    // Mysql
 export const MYSQL_LIST = 'cdb2/DescribeDBInstances' // 列表
-// 云硬盘
+    // 云硬盘
 export const DISK_LIST = 'cbs2/DescribeDisks' // 列表
-//负载均衡
+    //负载均衡
 export const CLB_LIST = 'lb2/DescribeLoadBalancers' // 列表
-// NAT网关
-// NAT网关
+    // NAT网关
+
 export const NAT_LIST = 'vpc/DescribeNatGateway' // 列表
-// 对等连接
+    // 对等连接
 export const PEE_LIST = 'vpc/DescribeVpcPeeringConnections' // 列表
-// 云数据库Redis
+    // 云数据库Redis
 export const REDIS_LIST = 'redis2/DescribeInstances' // 列表
 
 // VPN网关
@@ -27,3 +27,6 @@ export const VPNTD_LIST = 'vpc/DescribeVpnConn' // 列表
 
 // 对象存储
 export const OBJ_LIST = 'bucket/list' // 列表
+
+//弹性公网IP
+export const NETIP_LIST = 'vpc2/DescribeAddresses' // 列表
