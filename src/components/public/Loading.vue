62b241356d756bca70e2004cceb89e72a28669e0
13698810327@163.com <13698810327@163.com>
2019-11-28 10:33:59 +0800

    loading加载

---
+++ src/components/public/Loading.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrap" v-show="show">
+    <div class="box">
+      <i class="el-icon-loading"></i>
+      <p>Loading...</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "loading",
+  data() {
+    return {};
+  },
+  props: {
+    show: {
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.wrap {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .box {
+    width: 200px;
+    margin: 0 auto;
+    text-align: center;
+  }
+  p {
+    color: #65a5f9;
+    margin-top: 10px;
+  }
+  i {
+    color: #65a5f9;
+    font-size: 40px;
+  }
+}
+</style>
