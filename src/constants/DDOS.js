00b7beb6a32b7416a882df7dae47eb8d6ace2278
wangmingyang <wmy_near@163.com>
2019-12-04 10:57:49 +0800

    DDOS防护-防护概览

--- src/constants/DDOS.js
+++ src/constants/DDOS.js
@@ -2,7 +2,9 @@
     // 防护概览
 export const DDOS_SECINDEX = 'dayu2/DescribeSecIndex' // 获取安全统计-本月
 
+export const DDOS_PACKINDEX = 'dayu2/DescribePackIndex' // 获取产品总览
 
+export const DDOS_EV_LIST = 'dayu2/DescribeDDoSEvList' // 获取DDoS攻击事件列表
 
 
 
