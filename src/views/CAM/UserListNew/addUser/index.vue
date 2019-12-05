bbf4c9bee703d61e39351c7e4c6f9cd2104c75b1
13698810327@163.com <13698810327@163.com>
2019-12-05 12:36:16 +0800

    CAM新增用户

---
+++ src/views/CAM/UserListNew/addUser/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="addUser-wrap">
+    <HeadCom title="新增用户" :backShow='true'/>
+  </div>
+</template>
+
+<script>
+import HeadCom from "../components/Head";
+export default {
+  name: "addUser",
+  data() {
+    return {};
+  },
+  components: {
+    HeadCom
+  }
+};
+</script>
+
+<style scoped lang='scss'>
+</style>
