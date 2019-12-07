857cc7e8a8b58674fd02049209cec93416eb8761
zhaobobin <254787741@qq.com>
2019-12-07 11:20:57 +0800

    kms 简转繁

--- src/views/KMS/language/jt.js
+++ src/views/KMS/language/jt.js
@@ -1,8 +1,3 @@
 module.exports = {
-  DDOS: {
-    total: {
-      title: '密钥管理系统',
-      btn: 'Cloud Virtual Machine'
-    }
-  }
+
 }
--- src/views/KMS/main.js
+++ src/views/KMS/main.js
@@ -10,7 +10,7 @@ import VueCookie from 'vue-cookie'
 import '@/assets/iconfont/iconfont.css'
 import filters from '@/filters/filters.js'
 import echarts from 'echarts'
-import i18n from '@/language/i18n.js' // 引入i18n包
+import i18n from './language/i18n.js' // 引入i18n包
 // 引入基本模板
 //const echarts = require('echarts/lib/echarts')
 // 引入折线图组件
