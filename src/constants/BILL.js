9a8e184f075fb3e470fb7c6d5c9989e583fd5355
邓涛 <taodengzhy@163.com>
2019-12-07 16:37:25 +0800

    账单

---
+++ src/constants/BILL.js
@@ -0,0 +1,3 @@
+export const PROJECT_LIST = 'account/DescribeProject'      // 项目列表
+
+export const BILL_LIST = 'taifucloud/torderproduct/list'      // 订单列表
--- src/views/BILL/main.js
+++ src/views/BILL/main.js
@@ -11,12 +11,9 @@ import '@/assets/iconfont/iconfont.css'
 import filters from '@/filters/filters.js'
 import i18n from './language/i18n.js' // 引入i18n包
 
-// 引入基本模板
-const echarts = require('echarts/lib/echarts')
-    // 引入折线图组件
-require('echarts/lib/chart/line')
-    // 引入提示框和title组件，图例
-require('echarts/lib/component/tooltip')
+const echarts = require('echarts/lib/echarts')  // 引入基本模板
+require('echarts/lib/chart/line')   // 引入折线图组件
+require('echarts/lib/component/tooltip')  // 引入提示框和title组件，图例
 Vue.prototype.$echarts = echarts
 for (let key in filters) {
     Vue.filter(key, filters[key])
