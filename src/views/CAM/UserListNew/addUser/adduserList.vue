c4f95f73c3f194fab8da6d26074049a56c569832
13698810327@163.com <13698810327@163.com>
2019-12-11 13:51:18 +0800

    CAM新建子用户uin

--- src/views/CAM/UserListNew/addUser/adduserList.vue
+++ src/views/CAM/UserListNew/addUser/adduserList.vue
@@ -258,6 +258,19 @@ export default {
     this.init();
   },
   methods: {
+    //获取cookir
+    getCookie(name) {
+      var strcookie = document.cookie; //获取cookie字符串
+      var arrcookie = strcookie.split("; "); //分割
+      //遍历匹配
+      for (var i = 0; i < arrcookie.length; i++) {
+        var arr = arrcookie[i].split("=");
+        if (arr[0] == name) {
+          return arr[1];
+        }
+      }
+      return "";
+    },
     //返回上一级
     _back() {
       this.$router.go(-1);
@@ -407,7 +420,7 @@ export default {
         .then(() => {
           const params = {
             Password: this.taifuAIP.Password,
-            QcloudUin: this.taifuAIP.Uin,
+            QcloudUin: this.getCookie("uuid"), //uin
             SecretId: this.taifuAIP.SecretId,
             SecretKey: this.taifuAIP.SecretKey,
             SubAccountUin: this.taifuAIP.Uin,
