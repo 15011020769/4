fb9b3a2d091bd971bf5d8fd5d13da515ac3950cb
yanqianfan <qianfan.yan@seaboxdata.com>
2019-12-14 17:45:23 +0800

    WAF

---
+++ src/views/WAF/webFirewall/components/dominList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="dominList">
+      123
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.newClear:after{
+  display: inline-block;
+  content:'';
+  clear:both;
+}
+.dominList{
+  width:100%;
+  min-height:100px;
+  background-color:#fff;
+  margin-top:20px;
+  box-shadow: 0 2px 3px 0 rgba(0,0,0,.2);
+  padding: 20px;
+}
+</style>
---
+++ src/views/WAF/webFirewall/model/buyDominPack.vue
@@ -0,0 +1,113 @@
+<template>
+  <div>
+    <div>
+      <el-dialog
+        title="扩展域名包"
+        :visible.sync="buyDominPack"
+        width="45%"
+        :before-close="handleClose">
+        <div class="newClear">
+          <div class="newClear dominPackList">
+            <p>购买数量</p>
+            <p>
+              <el-input-number v-model="buyNum" @change="handleChange"></el-input-number>
+            </p>
+          </div>
+          <div class="newClear dominPackList">
+            <p>到期时间</p>
+            <p>2020-01-12 16：00:00</p>
+          </div>
+          <div class="newClear dominPackList">
+            <p>说明</p>
+            <p>每个域名包包含10个域名防护，仅支持1个一级域名；一次最多购买500个。</p>
+          </div>
+          <div class="newClear dominPackList">
+            <p>费用</p>
+            <p class="totalMoney">9,534.24元</p>
+          </div>
+        </div>
+        <span slot="footer" class="dialog-footer">
+          <el-button class="buyImmediate" @click="buyImmediate">立即购买</el-button>
+          <el-button @click="handleClose">取 消</el-button>
+        </span>
+      </el-dialog>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props:{
+    isShow:Boolean
+  },
+  data(){
+    return{
+      dialogModel:'',//弹框
+      buyNum:'5',//购买数量
+    }
+  },
+  computed:{
+    buyDominPack(){
+      this.dialogModel=this.isShow;
+      return this.isShow;
+    }
+  },
+  methods:{
+    //关闭按钮
+    handleClose(){
+      this.dialogModel=false;
+      this.$emit("buyDominPacModelClose",this.dialogModel)
+    },
+    //立即购买按钮
+    buyImmediate(){
+      this.$emit("buyDominPacModelClose",this.dialogModel)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.newClear:after{
+  content:'';
+  display:block;
+  clear:both;
+}
+::v-deep .el-dialog__title{
+  font-size:14px;
+  font-weight:600;
+}
+::v-deep .el-dialog__footer{
+  text-align:center;
+}
+::v-deep button.buyImmediate{
+  background-color:#ff9700;
+  color:#fff;
+  border:none;
+}
+::v-deep button{
+  height:30px;
+  line-height:30px;
+  border-radius: 0;
+}
+::v-deep .el-input__inner{
+  height:32px;
+}
+::v-deep .el-input-number{
+  line-height: 30px;
+}
+.dominPackList{
+  margin-bottom:20px;
+  p:nth-child(1){
+    float:left;
+    width:68px;
+    font-size:12px;
+    color:#999;
+  }
+  p:nth-child(2){
+    float:left;
+    width:calc(100% - 68px);
+  }
+  .totalMoney{
+    font-size:24px;
+    color:#ff7800;
+  }
+}
+</style>
