b3f553c93c512af23a5e09d5161082973c497037
13698810327@163.com <13698810327@163.com>
2019-12-24 16:38:38 +0800

    CM

--- src/views/CM/CLBload/index.vue
+++ src/views/CM/CLBload/index.vue
@@ -2,7 +2,7 @@
   <div class="CM-wrap">
     <!-- <Loading :show="loadShow" /> -->
     <!-- 城市按钮 -->
-    <div class="CVM-title">{{ $t('CVM.clBload.fzjh') }}</div>111
+    <div class="CVM-title">{{ $t('CVM.clBload.fzjh') }}</div>
     <div class="tool">
       <Cities :cities="cities" class="city" :Cityvalue.sync="selectedRegion" @changeCity="changeCity" />
       <!-- 搜索 -->
