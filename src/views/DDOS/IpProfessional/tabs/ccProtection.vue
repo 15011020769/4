f0533af1e8dc4b7ef0850ff088d9dcc7161bc802
wangmingyang <wmy_near@163.com>
2019-12-24 11:19:00 +0800

    撤销 "updateCode"
    这将还原提交 2598fb8cf48a2d5cf565f368b5005a40ff687def

---
+++ src/views/DDOS/IpProfessional/tabs/ccProtection.vue
@@ -0,0 +1,355 @@
+<template>
+  <div id="ccProtection">
+    <div class="ccProtectPartOne">
+      <el-select class="ccResourceId" v-model="ccResourceId" filterable placeholder="请选择">
+        <el-option
+          v-for="item in resourceIds"
+          :key="item"
+          :label="item"
+          :value="item"
+        ></el-option>
+      </el-select>
+    </div>
+    <div class="ccProtectPartTwo">
+      <h2>{{$t('DDOS.Proteccon_figura.HTTP_protection')}}</h2>
+      <div class="partTwoIpt">
+        <div class="newClear">
+          <p class="partTwoPO">{{$t('DDOS.Proteccon_figura.Protection_state')}}</p>
+          <p class="partTwoPT">
+            <el-switch
+              class="switch"
+              v-model="switchState"
+              active-color="#006eff"
+              inactive-color="#bbb"
+            ></el-switch>
+            <span class="switchTip">{{$t('DDOS.Proteccon_figura.Sensitive_services')}}</span>
+          </p>
+        </div>
+        <div class="newClear" v-if="switchState==true?true:false">
+          <p class="partTwoPO">http请求数阈值</p>
+          <p class="partTwoPT">
+            <el-select class="ccProtectSele" v-model="httpRequestNum" placeholder="请选择">
+              <el-option
+                v-for="item in httpOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>
+            </el-select>
+            <span class="marginLeftSpan">{{$t('DDOS.Proteccon_figura.numbe_HTTP')}}</span>
+          </p>
+        </div>
+        <div class="newClear" v-if="switchState==true?true:false">
+          <p class="partTwoPO">HTTP CC攻击告警阈值</p>
+          <p class="partTwoPT">
+            <el-input v-model="httpCcNum" class="partTwoPTIpt"></el-input>
+            <span class="marginLeftSpan">QPS</span>
+          </p>
+          <span class="botTop">{{$t('DDOS.Proteccon_figura.CC_classified')}}</span>
+        </div>
+      </div>
+    </div>
+    <div>
+      <addAccessControl :ccResourceId="ccResourceId" />
+    </div>
+    <div>
+      <addIpList :resourceId="ccResourceId" :ccUrlWhiteList="ccUrlWhiteList" />
+    </div>
+  </div>
+</template>
+<script>
+import addAccessControl from "./addAccessControl";
+import addIpList from "./addIpList";
+import {
+  RESOURCE_LIST,
+  CC_THRESHOLD,
+  CC_URLALLOW,
+  DDOSPOLICY_CONT,
+  CCSELFDEFINEPOLICY_CREATE
+} from "@/constants";
+export default {
+  props: {
+    // ccResourceId: String //第一部分下拉 资源ID
+  },
+  data() {
+    return {
+      ccResourceId: '',
+      loading: true,
+      resourceIds: [], //下拉内容
+      switchState: true, //防护状态
+      httpRequestNum: 350, //http请求阈值
+      httpOptions: [
+        {
+          value: 50,
+          label: "50QPS"
+        },
+        {
+          value: 100,
+          label: "100QPS"
+        },
+        {
+          value: 150,
+          label: "150QPS"
+        },
+        {
+          value: 240,
+          label: "240QPS"
+        },
+        {
+          value: 350,
+          label: "350QPS"
+        },
+        {
+          value: 480,
+          label: "480QPS"
+        },
+        {
+          value: 550,
+          label: "550QPS"
+        },
+        {
+          value: 700,
+          label: "700QPS"
+        },
+        {
+          value: 850,
+          label: "850QPS"
+        },
+        {
+          value: 1000,
+          label: "1000QPS"
+        },
+        {
+          value: 1500,
+          label: "1500QPS"
+        },
+        {
+          value: 2000,
+          label: "2000QPS"
+        },
+        {
+          value: 3000,
+          label: "3000QPS"
+        },
+        {
+          value: 5000,
+          label: "5000QPS"
+        },
+        {
+          value: 10000,
+          label: "10000QPS"
+        },
+        {
+          value: 20000,
+          label: "20000QPS"
+        }
+      ], //http请求阈值数据
+      httpCcNum: 1000, //HTTP CC攻击告警阈值
+      // ccPolicy: {}, //创建CC自定义策略对象
+      ccUrlWhiteList: [] //CC的Url白名单
+    };
+  },
+  components: {
+    addAccessControl: addAccessControl, //访问策略控制表格
+    addIpList: addIpList //添加IP表格
+  },
+  created() {
+    this.getData();
+  },
+  watch: {
+    switchState: function(value) {
+      this.modifyCCThreshold();
+      this.describeResourceList();
+      this.describeCCUrlAllow();
+    },
+    httpRequestNum: function(value) {
+      this.modifyCCThreshold();
+      this.describeResourceList();
+      this.describeCCUrlAllow();
+    }
+  },
+  methods: {
+    getData() {
+      this.initResourceList();
+    },
+    // 1.0.初始化资源列表，获取resourceIds
+    initResourceList() {
+      let params = {
+        Version: "2018-07-09",
+        Business: "net"
+      };
+      this.axios.post(RESOURCE_LIST, params).then(res => {
+        for (let i = 0; i < res.Response.ServicePacks.length; i++) {
+          let list = res.Response.ServicePacks[i];
+          list.Record.forEach((value, index) => {
+            if (value.Key == "Id") {
+              this.resourceIds.push(value.Value);
+            }
+          });
+        }
+        this.ccResourceId = this.resourceIds[0];
+      });
+    },
+    // 1.1.获取资源列表
+    describeResourceList() {
+      let params = {
+        Version: "2018-07-09",
+        Business: "net",
+        "IdList.0": this.ccResourceId
+      };
+      this.axios.post(RESOURCE_LIST, params).then(res => {
+        console.log(params, res);
+      });
+    },
+    // 1.2.获取CC的Url白名单
+    describeCCUrlAllow() {
+      let params = {
+        Version: "2018-07-09",
+        Business: "net",
+        Id: this.ccResourceId,
+        "Type.0": "white"
+      };
+      this.axios.post(CC_URLALLOW, params).then(res => {
+        // console.log(res);
+        // this.ccUrlWhiteList = res.Response
+      });
+    },
+    // 1.3.修改CC的防护阈值
+    modifyCCThreshold() {
+      let params = {
+        Version: "2018-07-09",
+        Business: "net",
+        Id: this.ccResourceId,
+        Protocol: this.switchState ? "http" : "",
+        Threshold: this.switchState ? this.httpRequestNum : 0
+      };
+      this.axios.post(CC_THRESHOLD, params).then(res => {
+        console.log(params, res);
+      });
+    },
+    
+    // 1.5.获取DDoS高级策略(未调用)
+    describeDDoSPolicy() {
+      let params = {
+        Version: "2018-07-09",
+        Business: "net"
+      };
+      this.axios.post(DDOSPOLICY_CONT, params).then(res => {
+        console.log(res);
+      });
+    }
+  }
+};
+</script>
+<style lang="scss">
+#ccProtection {
+  .newClear:after {
+    display: block;
+    clear: both;
+    content: "";
+  }
+  .ccProtectPartOne {
+    width: 100%;
+    height: 80px;
+    padding: 20px;
+    background-color: #fff;
+    border: 1px solid #ddd;
+    box-shadow: 0 2px 3px 0 rgba(0, 0, 0, 0.2);
+    margin-bottom: 20px;
+    .ccProtectSele {
+      width: 180px;
+      height: 30px;
+      div {
+        width: 180px;
+        height: 30px;
+        input {
+          width: 180px;
+        }
+      }
+    }
+  }
+  .ccProtectPartTwo {
+    width: 100%;
+    padding: 20px;
+    background-color: #fff;
+    border: 1px solid #ddd;
+    box-shadow: 0 2px 3px 0 rgba(0, 0, 0, 0.2);
+    margin-bottom: 20px;
+    h2 {
+      font-size: 14px;
+      font-weight: 600;
+      color: #000;
+      margin-bottom: 20px;
+    }
+    .partTwoIpt {
+      div {
+        margin-bottom: 16px;
+        p:nth-child(1).partTwoPO {
+          float: left;
+          width: 140px;
+          font-size: 12px;
+          color: #999;
+        }
+        p:nth-child(2).partTwoPT {
+          float: left;
+          width: calc(100% - 140px);
+          font-size: 12px;
+          color: #999;
+          .switch {
+            float: left;
+          }
+          .switchTip {
+            margin-left: 12px;
+            float: left;
+            margin-top: 3px;
+          }
+          .marginLeftSpan {
+            margin-left: 20px;
+          }
+          .partTwoPTIpt {
+            width: 180px;
+            input {
+              width: 180px;
+            }
+          }
+        }
+        .botTop {
+          color: #bbb;
+        }
+      }
+    }
+  }
+  .el-select {
+    height: 30px !important;
+  }
+  .el-input {
+    height: 30px !important;
+  }
+  .el-input__inner {
+    border-radius: 0 !important;
+    height: 30px !important;
+  }
+  .buttonGroupAll {
+    float: left;
+    button {
+      height: 30px;
+      line-height: 30px;
+      padding: 0 16px;
+      border-radius: 0;
+    }
+  }
+  .newDataTime {
+    float: left;
+    height: 30px;
+    line-height: 30px;
+    border-radius: 0;
+    .el-input__icon {
+      line-height: 26px;
+    }
+    .el-range-separator {
+      line-height: 26px;
+      width: 7%;
+    }
+  }
+}
+</style>
