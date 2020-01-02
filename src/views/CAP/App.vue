b08d108814c43aeef26554ddfadd8fd2fa192c29
zhaobobin <254787741@qq.com>
2020-01-02 16:43:48 +0800

    city

--- src/views/CAP/App.vue
+++ src/views/CAP/App.vue
@@ -5,8 +5,7 @@
         <nav-header></nav-header>
       </el-header>
       <el-container height="100%">
-        <el-aside width="200px"
-          height="100%">
+        <el-aside width="200px" height="100%">
           <left-aside></left-aside>
         </el-aside>
         <el-main>
@@ -19,62 +18,61 @@
 </template>
 
 <script>
-import NavHeader from '@/components/HeaderAside/Header'
-import LeftAside from './Public/Menu'
+  import NavHeader from '@/components/HeaderAside/Header'
+  import LeftAside from './Public/Menu'
+  import {
+    ALL_CITY
+  } from "@/constants";
 
-export default {
-  data () {
-    return {}
-  },
-  components: {
-    NavHeader,
-    LeftAside
-  },
-  mounted () {
-    // this.getProgectList()
-    if (this.$cookie.get('regionv1') === undefined || this.$cookie.get('regionv1') === '' || this.$cookie.get('regionv1') === null) {
-      this.$cookie.set('regionv1', 'gz')
-      this.$cookie.set('regionv2', 'ap-guangzhou')
-    }
-  },
-  methods: {
-    // 获取项目列表
-    getProgectList () {
-      var params = {}
-      this.axios.post('account/DescribeProject', params).then(data => {
-        let projectList = data.data
-        let defaultPro = {
-          projectId: 0,
-          projectInfo: '默认项目',
-          projectName: '默认项目'
-        }
-        projectList.unshift(defaultPro)
-        // localStorage存储对象或者数据，必须先转成字符串JSON.stringify存储，在用JSON.parse进行解析
-        localStorage.setItem('projectList', JSON.stringify(projectList))
-      })
+  export default {
+    data() {
+      return {}
+    },
+    components: {
+      NavHeader,
+      LeftAside
+    },
+    mounted() {
+      this.GetCity();
+    },
+    methods: {
+      // 获取城市列表
+      GetCity() {
+        this.axios.get(ALL_CITY).then(data => {
+          let city = data.data[0];
+          localStorage.setItem("regionv1", city.regionCode);
+          localStorage.setItem("regionv2", city.Region);
+        });
+      }
     }
   }
-}
+
 </script>
 <style lang="scss" scoped>
-#CAP >>> .el-aside {
-  background: #292b36 !important;
-}
-#CAP {
-  height: 100%;
-  .el-container {
+  #CAP>>>.el-aside {
+    background: #292b36 !important;
+  }
+
+  #CAP {
     height: 100%;
-    .el-header {
-      padding: 0 0 !important;
-    }
-    .el-aside {
-      background-color: rgb(84, 92, 100);
-    }
-    .el-main {
-      padding: 0;
-      background: #f2f2f2;
+
+    .el-container {
       height: 100%;
+
+      .el-header {
+        padding: 0 0 !important;
+      }
+
+      .el-aside {
+        background-color: rgb(84, 92, 100);
+      }
+
+      .el-main {
+        padding: 0;
+        background: #f2f2f2;
+        height: 100%;
+      }
     }
   }
-}
+
 </style>
--- src/views/CCN/App.vue
+++ src/views/CCN/App.vue
@@ -18,52 +18,61 @@
 </template>
 
 <script>
-import NavHeader from "@/components/HeaderAside/Header";
-import LeftAside from "./Public/Menu";
-import { ALL_PROJECT, ALL_CITY } from "@/constants";
-export default {
-  data() {
-    return {};
-  },
-  components: {
-    NavHeader,
-    LeftAside
-  },
-  mounted() {
-    this.GetCity();
-  },
-  methods: {
-    // 获取城市列表
-    GetCity() {
-      this.axios.get(ALL_CITY).then(data => {
-        let city = data.data[0];
-        localStorage.setItem("regionv1", city.regionCode);
-        localStorage.setItem("regionv2", city.Region);
-      });
+  import NavHeader from "@/components/HeaderAside/Header";
+  import LeftAside from "./Public/Menu";
+  import {
+    ALL_CITY
+  } from "@/constants";
+  export default {
+    data() {
+      return {};
+    },
+    components: {
+      NavHeader,
+      LeftAside
+    },
+    mounted() {
+      this.GetCity();
+    },
+    methods: {
+      // 获取城市列表
+      GetCity() {
+        this.axios.get(ALL_CITY).then(data => {
+          let city = data.data[0];
+          localStorage.setItem("regionv1", city.regionCode);
+          localStorage.setItem("regionv2", city.Region);
+        });
+      }
     }
-  }
-};
+  };
+
 </script>
 <style lang="scss" scoped>
-#CCN >>> .el-aside {
-  background: #292b36 !important;
-}
-#CCN {
-  height: 100%;
-  font-size: 12px;
-  .el-container {
+  #CCN>>>.el-aside {
+    background: #292b36 !important;
+  }
+
+  #CCN {
     height: 100%;
-    .el-header {
-      padding: 0 0 !important;
-    }
-    .el-aside {
-      background-color: #292b36;
-    }
-    .el-main {
-      padding: 0;
-      background: #f2f2f2;
+    font-size: 12px;
+
+    .el-container {
       height: 100%;
+
+      .el-header {
+        padding: 0 0 !important;
+      }
+
+      .el-aside {
+        background-color: #292b36;
+      }
+
+      .el-main {
+        padding: 0;
+        background: #f2f2f2;
+        height: 100%;
+      }
     }
   }
-}
+
 </style>
