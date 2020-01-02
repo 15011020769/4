68869e85c1eb9c7d6fa7de9e8d6dd2ecf2b71c29
13698810327@163.com <13698810327@163.com>
2020-01-02 15:21:41 +0800

    CM监控概览

--- src/views/CM/router.js
+++ src/views/CM/router.js
@@ -11,7 +11,7 @@ export default new Router({
   },
   routes: [{
     path: '/',
-    redirect: '/CVM'
+    redirect: '/overview'
   },
   {
     path: '/CVM', // 雲服务器
--- src/views/CM/view/index.vue
+++ src/views/CM/view/index.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="view-wrap">监控视图</div>
+  <div class="view-wrap">
+    <Header title="监控视图"/>
+  </div>
 </template>
 
 <script>
+import Header from "@/components/public/Head";
 export default {
   name: "view",
   data() {
     return {};
+  },
+  components: {
+    Header
   }
 };
 </script>
