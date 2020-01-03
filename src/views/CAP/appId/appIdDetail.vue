8e16818ec809647c0694214cd9f84186f9b3c732
ji4396 <1187939346@qq.com>
2020-01-03 09:03:50 +0800

    xinzeng

--- src/views/CAP/appId/appIdDetail.vue
+++ src/views/CAP/appId/appIdDetail.vue
@@ -1,1 +1,24 @@
 
+<template>
+    <div class="">
+
+    </div>
+</template>
+<script>
+export default {
+   data() {
+      return {
+
+      }
+   },
+   mounted() {
+
+   },
+   methods:{
+
+   },
+}
+</script>
+<style scoped>
+/* css */
+</style>
