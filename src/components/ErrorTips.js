58b305cdd028c70a1aab105bffc28ab3e68afa65
zhaobobin <254787741@qq.com>
2020-01-06 18:03:57 +0800

    ccn 接口

---
+++ src/components/ErrorTips.js
@@ -0,0 +1,31 @@
+let ErrorTips = {
+  'AuthFailure.InvalidSecretId': '密钥非法（不是云 API 密钥类型',
+  'AuthFailure.MFAFailure': 'MFA 错误',
+  'AuthFailure.SecretIdNotFound': '密钥不存在',
+  'AuthFailure.SignatureExpire': '签名过期',
+  'AuthFailure.SignatureFailure': '签名错误',
+  'AuthFailure.TokenFailure': 'token 错误',
+  'AuthFailure.UnauthorizedOperation': '请求未 CAM 授权',
+  'DryRunOperation': 'DryRun 操作，代表请求将会是成功的，只是多传了 DryRun 参数',
+  'FailedOperation': '操作失败',
+  'InternalError': '内部错误',
+  'InvalidAction': '接口不存在',
+  'InvalidParameter': '参数错误',
+  'InvalidParameterValue': '参数取值错误',
+  'LimitExceeded': '超过配额限制',
+  'MissingParameter': '缺少参数错误',
+  'NoSuchVersion': '接口版本不存在',
+  'RequestLimitExceeded': '请求的次数超过了频率限制',
+  'ResourceInUse': '资源被占用',
+  'ResourceInsufficient': '资源不足',
+  'ResourceNotFound': '资源不存在',
+  'ResourceUnavailable': '资源不可用',
+  'UnauthorizedOperation': '未授权操作',
+  'UnknownParameter': '未知参数错误',
+  'UnsupportedOperation': '操作不支持',
+  'UnsupportedProtocol': 'HTTPS 请求方法错误，只支持 GET 和 POST 请求',
+  'UnsupportedRegion': '接口不支持所传地域'
+}
+export {
+  ErrorTips
+}
