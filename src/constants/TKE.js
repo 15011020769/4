a81c1be2fb7ef92bdd944b2bdd1ad835e0ffdb21
idboom <wolilong@gmail.com>
2020-01-06 21:01:38 +0800

    集群列表接口联条，详情页静态页面编写

--- src/constants/TKE.js
+++ src/constants/TKE.js
@@ -1,5 +1,8 @@
 // 我的镜像
 export const CGI_LIST = 'tke2/capi' 
 
-// 获取容器列表
+// 获取集群列表
 export const COLONY_LIST = 'tke2/DescribeClusters' 
+// 获取集群列表状态
+export const COLONY_STATUS = 'tke2/DescribeClusterStatus'
+
