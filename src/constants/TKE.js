a3de8b032902324b48510c900ab031f585934dde
wangyuren <wyr1996cc@163.com>
2020-01-07 19:58:44 +0800

    完成我的镜像和命名空间的接口（50%）

--- src/constants/TKE.js
+++ src/constants/TKE.js
@@ -1,11 +1,21 @@
-// 命名空间
-export const SPACENAME_LIST = 'ccr/GetNamespaceInfo'
-
-// 我的镜像
+// 获取我的镜像
 export const MIRROR_LIST = 'ccr/SearchUserRepository'
 
+// 获取我的镜像删除
+export const MIRROR_DELETE = 'ccr/BatchDeleteRepository'
+
+// 判断镜像名称是否存在
+export const MIRROR_PRESENCE = 'ccr/RepositoryisExists'
+
+// 创建我的镜像数据
+export const MIRROR_CREATE = 'ccr/CreateRepository'
+
+// 获取命名空间
+export const SPACENAME_LIST = 'ccr/GetNamespaceInfo'
+
 // 获取集群列表
-export const COLONY_LIST = 'tke2/DescribeClusters' 
+export const COLONY_LIST = 'tke2/DescribeClusters'
+
 // 获取集群列表状态
 export const COLONY_STATUS = 'tke2/DescribeClusterStatus'
 // 获取集群基本信息相关
