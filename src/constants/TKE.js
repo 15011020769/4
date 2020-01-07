3e36518e86ff35bcf0bbdaf73dc90c4d6403f5d8
hhl <570762846@qq.com>
2020-01-07 21:02:47 +0800

    接口调试中

--- src/constants/TKE.js
+++ src/constants/TKE.js
@@ -1,23 +1,15 @@
-// 获取我的镜像
-export const MIRROR_LIST = 'ccr/SearchUserRepository'
-
-// 获取我的镜像删除
-export const MIRROR_DELETE = 'ccr/BatchDeleteRepository'
-
-// 判断镜像名称是否存在
-export const MIRROR_PRESENCE = 'ccr/RepositoryisExists'
-
-// 创建我的镜像数据
-export const MIRROR_CREATE = 'ccr/CreateRepository'
-
-// 获取命名空间
-export const SPACENAME_LIST = 'ccr/GetNamespaceInfo'
+// 我的镜像
+export const CGI_LIST = 'tke2/capi' 
 
 // 获取集群列表
-export const COLONY_LIST = 'tke2/DescribeClusters'
-
+export const COLONY_LIST = 'tke2/DescribeClusters' 
 // 获取集群列表状态
 export const COLONY_STATUS = 'tke2/DescribeClusterStatus'
-// 获取集群基本信息相关
-export const CLUSTERS_DESCRIBE = 'tke2/DescribeClusters'
-export const CLUSTERS_SECURITY = 'tke2/DescribeClusterSecurity'
+// 获取警告设置集群列表
+export const WARNING_GetUSER = 'tke2/DescribeClusters'
+// 获取告警设置集群详细信息
+export const WARNING_GetCOLONY = 'tke2/DescribeAlarmPolicies'
+// 获取用户组列表信息
+export const CreateListGroups = 'cam/ListGroups'
+// 获取用户组信息
+export const CreateGetGroups = 'cam/Get'
--- src/views/TKE/warnings/index.vue
+++ src/views/TKE/warnings/index.vue
@@ -9,6 +9,13 @@
       <div style="width:20px"></div>
       <div style="padding-top:3px;">集群</div> 
       &nbsp;
+      <el-select size="mini" v-model="value" placeholder="请选择" @change="GetCOLONY_name()">
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :value="item.value">
+        </el-option>
+      </el-select>
     </div>
     <div class="app-tke-fe-content__inner">
       <!-- 搜索框与新建，删除 -->
@@ -42,29 +49,40 @@
         </div>
         <div class="ep-data-card-main" style="padding-top:20px;">
           <el-row>
-            <el-col :span="5"><div class="font">
+            <el-col :span="5"><div class="font pt12">
                 <input type="checkbox" class="app-tke-fe-checkbox"><a href="javascript:;">aaaaaaa</a>
               </div></el-col>
-            <el-col :span="4"><div class="font">集群</div></el-col>
+            <el-col :span="4"><div class="font pt12">集群</div></el-col>
             <el-col :span="5"><div class="font">
                 <div>CPU利用率>90%,持续5分钟告警</div>
                 <div>内存利用率>90%,持续5分钟告警</div>
                 <div>CPU分配率>90%,持续5分钟告警</div>
               </div></el-col>
-            <el-col :span="5"><div class="font">
+            <el-col :span="5"><div class="font pt6">
                 <div>接收组:1个</div>
                 <div>渠道:短信</div>
               </div></el-col>
-            <el-col :span="5"><div class="font">
-              <router-link :to="'/persistenceSetting/'+uid">
+            <el-col :span="5"><div class="font pt12">
+              <router-link :to="''">
                 <span>删除</span>
               </router-link>
               <div style="width:10px;display:inline-block"></div>
-               <router-link :to="'/persistenceSetting/'+uid">
+               <router-link :to="''">
                 <span>复制</span>
               </router-link>
               </div></el-col>
           </el-row>
+        </div>
+        <div class="font flex">
+          <div style="flex:1;padding-top:17px;">共&nbsp;1&nbsp;项</div>
+          <div class="block" style="padding-top:5px;">
+            <el-pagination
+              :page-sizes="[10, 20, 30, 40, 50]"
+              :page-size="20"
+              layout="sizes, prev, pager, next"
+              :total="100">
+            </el-pagination>
+          </div>
         </div>
       </div>
     </div>
@@ -72,17 +90,136 @@
 </template>
 
 <script>
+import {
+  CreateGetGroups,
+  CreateListGroups,
+  WARNING_GetCOLONY,
+  WARNING_GetUSER
+} from "@/constants";
 export default {
   name:'warnings',
-  data(){
-    return{
-
+  data() {
+    return {
+      ClusterId: 'cls-gwblk71e',
+      pageSize: 20,
+      pageIndex: 0,
+      options: [],
+      value: ''
     }
+  },
+  watch:{
+    value(val) {
+      let params = {
+        ClusterInstanceId: (val.split('('))[0],
+        Limit: this.pageSize,
+        Offset: this.pageIndex,
+        Version: "2018-05-25"
+      }
+      console.log((val.split('('))[0])
+      const res = this.axios.post(WARNING_GetCOLONY,params).then(res=>{
+        if(res.Response.AlarmPolicySet.length>0){
+          // console.log(res)
+          
+        }else{
+          console.log('数据请求出错')
+        }
+      });
+      // if(res.Error){
+      //   console.log(res);
+      // }else{
+      //   console.log(res)
+        // console.log(res.[[PromiseValue]].Response)
+        // if(res.Response.AlarmPolicySet.length>0){
+        //   consoel.log(1)
+        // }
+        // if(res.Response.Clusters.length>0){
+        //   let ids=[];
+        //   res.Response.Clusters = res.Response.Clusters.map(item => {
+        //       ids.push(item.ClusterId+'('+item.ClusterName+')');
+        //       return item;
+        // })
+      // }
+    }
+  },
+  created() {
+    // 获取集群列表
+    this.getWarningList();
+    // 获取集群列表详情
+    // this.getWarningListItem();
+  },
+  methods:{
+    // 获取集群列表
+    async getWarningList() {
+      let params = {
+          Limit: this.pageSize,
+          Offset: this.pageIndex,
+          Version: "2018-05-25"
+        }
+      // console.log(params)
+      const res = await this.axios.post(WARNING_GetUSER, params);
+      if(res.Error){
+        console.log(res)
+      }else{
+        // console.log(res)
+        if(res.Response.Clusters.length>0){
+          let ids=[];
+          res.Response.Clusters = res.Response.Clusters.map(item => {
+              ids.push(item.ClusterId+'('+item.ClusterName+')');
+              return item;
+          })
+          // 放到页面上
+          for (let i = 0; i < ids.length; i++) {
+            let option = {}
+            option.value = ids[i]
+            this.options.push(option)
+            this.value = this.options[0].value
+          }
+          // console.log(this.options)
+        }
+      }
+    },
+    // 获取集群列表详情
+    async getWarningListItem() {
+      let params = {
+          // ClusterInstanceId: this.options
+          Limit: this.pageSize,
+          Offset: this.pageIndex,
+          Version: "2018-05-25"
+        }
+      console.log(params)
+      const res = await this.axios.post(WARNING_GetUSER, params);
+      if(res.Error){
+        console.log(res)
+      }else{
+        if(res.Response.Clusters.length>0){
+          let ids=[];
+          res.Response.Clusters = res.Response.Clusters.map(item => {
+              ids.push(item.ClusterId+'('+item.ClusterName+')');
+              return item;
+          })
+          // 放到页面上
+          for (let i = 0; i < ids.length; i++) {
+            const element = ids[i];
+            this.options[i].value = i
+            this.options[i].label = ids[i]
+          }
+        }
+      }
+    },
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.pt20 {
+  padding-top: 20px;
+}
+.pt12 {
+  padding-top: 12px;
+}
+.pt6 {
+  padding-top: 6px;
+}
 .app-tke-fe-checkbox {
   width: 16px;
   height: 16px;
@@ -142,7 +279,7 @@ export default {
   display: flex;
 }
 .event-persistence {
-  padding: 20px;
+  padding: 20px 20px 10px 20px;
   background: white;
   box-shadow: 0 1px 3px 0 rgba(0,0,0,.1);
 }
