1d6fcde8bbf1aa47b9d7649769391ea9fb7d57d5
idboom <wolilong@gmail.com>
2020-01-14 16:55:35 +0800

    更新

--- src/views/TKE/colony/sub/detail/storage/sc/yaml.vue
+++ src/views/TKE/colony/sub/detail/storage/sc/yaml.vue
@@ -1,7 +1,14 @@
  <!-- StorageClass-yaml -->
 <template>
   <div class="colony-main">
-    <div class="tke-card tke-formpanel-wrap">
+    <div class="tke-grid ">
+      <!-- 左侧 -->
+      <div class="grid-left">
+        <el-button  size="small" type="primary">编辑YAML</el-button>
+      </div>
+    </div>
+    
+    <div class="tke-card mt10 tke-formpanel-wrap">
         yaml
     </div>
   </div>
