878352e73daf870b6e0e06babfd09ff0404a47cb
idboom <wolilong@gmail.com>
2020-01-14 19:38:09 +0800

    新增集群-存储相关详情页面及其子路

---
+++ src/views/TKE/colony/sub/detail/storage/pv/info.vue
@@ -0,0 +1,69 @@
+ <!-- PersistentVolume-基本信息 -->
+<template>
+  <div class="colony-main">
+    <div class="tke-card tke-formpanel-wrap">
+      <h4  class="tke-formpanel-title">基本信息</h4>
+      <el-form  class="tke-form" label-position='left' label-width="120px" size="mini">
+        <el-form-item label="名称">
+          <div class="tke-form-item_text">sdsd</div>
+        </el-form-item>
+        <el-form-item label="Labels">
+          <div class="tke-form-item_text">-</div>
+        </el-form-item>
+        <el-form-item label="状态">
+          <div class="tke-form-item_text"><span class="text-green">Bound</span></div>
+        </el-form-item>
+        <el-form-item label="访问权限">
+          <div class="tke-form-item_text">ReadWriteOnce</div>
+        </el-form-item>
+        <el-form-item label="PVC">
+          <div class="tke-form-item_text">vvv</div>
+        </el-form-item>
+        <el-form-item label="StorageClass">
+          <div class="tke-form-item_text">cbs</div>
+        </el-form-item>
+        <el-form-item label="Storage">
+          <div class="tke-form-item_text">10Gi</div>
+        </el-form-item>
+        <el-form-item label="回收策略">
+          <div class="tke-form-item_text">Retain</div>
+        </el-form-item>
+        <el-form-item label="创建时间">
+          <div class="tke-form-item_text">2020-01-02 14:02:26</div>
+        </el-form-item>
+      </el-form>
+    </div>
+   </div>
+</template>
+
+<script>
+import FileSaver from "file-saver";
+import XLSX from "xlsx";
+import { ALL_CITY } from "@/constants";
+export default {
+  name: "pvDetailInfo",
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+   
+  },
+  created() {
+     // 从路由获取类型
+   
+  },
+  methods: {
+   
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+
+
+
+</style>
+
---
+++ src/views/TKE/colony/sub/detail/storage/pv/yaml.vue
@@ -0,0 +1,45 @@
+ <!-- PersistentVolume-yaml -->
+<template>
+  <div class="colony-main">
+    <div class="tke-grid ">
+      <!-- 左侧 -->
+      <div class="grid-left">
+        <el-button  size="small" type="primary">编辑YAML</el-button>
+      </div>
+    </div>
+    
+    <div class="tke-card mt10 tke-formpanel-wrap">
+        yaml
+    </div>
+  </div>
+</template>
+
+<script>
+import FileSaver from "file-saver";
+import XLSX from "xlsx";
+import { ALL_CITY } from "@/constants";
+export default {
+  name: "pvDetailYaml",
+  data() {
+    return {
+     
+    };
+  },
+  components: {
+   
+  },
+  created() {
+     // 从路由获取类型
+   
+  },
+  methods: {
+   
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
+
