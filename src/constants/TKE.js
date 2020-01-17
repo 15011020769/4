2ba31f975bd64c7232a083008989ee70363eee86
wangyuren <wyr1996cc@163.com>
2020-01-17 15:47:56 +0800

    更新

--- src/constants/TKE.js
+++ src/constants/TKE.js
@@ -5,7 +5,7 @@ export const SPACENAME_DELETE = 'ccr/DeleteUserNamespace'
 export const SPACENAME_PRESENCE = 'ccr/NamespaceIsExists'
 
 // 创建命名空间
-export const CREATE_SPACENAME = 'ccr/CreateCCRNamespace'
+export const CREATE_SPACENAME = 'ccr/GetRepositoryInfo'
 
 // 获取我的镜像列表
 export const MIRROR_LIST = 'ccr/SearchUserRepository'
--- src/views/TKE/components/tkeSearch.vue
+++ src/views/TKE/components/tkeSearch.vue
@@ -105,7 +105,7 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" >
  .tke-download,.tke-refresh{
       font-size: 16px;
       padding: 8px;
