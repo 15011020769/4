be8d100278f684f0dd0da24176cb39d5289b86f9
idboom <wolilong@gmail.com>
2020-01-17 17:07:09 +0800

    更新

--- src/views/TKE/colony/sub/detail/resource/deployment/info.vue
+++ src/views/TKE/colony/sub/detail/resource/deployment/info.vue
@@ -2,85 +2,107 @@
 <template>
   <div class="colony-main">
     <div class="tke-card tke-formpanel-wrap">
-      <h4  class="tke-formpanel-title">主机信息</h4>
+      <h4  class="tke-formpanel-title">基本信息</h4>
       <el-form  class="tke-form" label-position='left' label-width="120px" size="mini">
-        <el-form-item label="节点ID">
-          <div class="tke-form-item_text"><span class="tke-text-link">	ins-5ic5rvb2</span></div>
+        <el-form-item label="名称">
+          <div class="tke-form-item_text"><span class="tke-text-link">kube-dns</span></div>
         </el-form-item>
-        <el-form-item label="状态">
-          <div class="tke-form-item_text"><span class="text-green">健康</span></div>
+        <el-form-item label="Namespace">
+          <div class="tke-form-item_text">kube-system</div>
         </el-form-item>
-        <el-form-item label="地域">
-          <div class="tke-form-item_text">港澳台地区(中国台北)</div>
-        </el-form-item>
-        <el-form-item label="可用区">
-          <div class="tke-form-item_text">台北一区</div>
-        </el-form-item>
-        <el-form-item label="配置">
-          <div class="tke-form-item_text">
-            <span class="text-gray">CPU:</span> 1 核，
-            <span class="text-gray">内存:</span> 1 GB，
-            <span class="text-gray">带宽:</span> 1 Mbps，
-            <span class="text-gray">系统盘:</span> 50 GB(高性能云硬盘)</div>
-        </el-form-item>
-        <el-form-item label="IP地址">
-          <div class="tke-form-item_text">
-            <p>175.97.136.110 (外网)</p>
-            <p>10.0.128.8 (内网)</p>
-          </div>
-        </el-form-item>
-        <el-form-item label="操作系统">
-          <div class="tke-form-item_text">Ubuntu 16.04.1 LTS</div>
+        <el-form-item label="描述">
+          <div class="tke-form-item_text">-</div>
         </el-form-item>
         <el-form-item label="创建时间">
           <div class="tke-form-item_text">2020-01-10 14:15:05</div>
+        </el-form-item>
+        <el-form-item label="Labels">
+          <div class="tke-form-item_text">addonmanager.kubernetes.io/mode：Reconcile、k8s-app：kube-dns、kubernetes.io/cluster-service：true</div>
+        </el-form-item>
+        <el-form-item label="Selector">
+          <div class="tke-form-item_text">k8s-app：kube-dns</div>
+        </el-form-item>
+        <el-form-item label="更新策略">
+          <div class="tke-form-item_text">RollingUpdate</div>
+        </el-form-item>
+        <el-form-item label="副本数">
+          <div class="tke-form-item_text">2</div>
+        </el-form-item>
+        <el-form-item label="运行副本数">
+          <div class="tke-form-item_text">1</div>
+        </el-form-item>
+        
+      </el-form>
+    </div>
+
+    <div class="tke-card tke-formpanel-wrap mt10">
+      <h4  class="tke-formpanel-title">数据卷（Volumes）</h4>
+      <el-form  class="tke-form" label-position='left' label-width="120px" size="mini">
+        <el-form-item label="configMap">
+          <div class="tke-form-item_text">kube-dns-config（卷名称） kube-dns（资源名称）</div>
+        </el-form-item>
+        <el-form-item label="configMap">
+          <div class="tke-form-item_text">clientconfig（卷名称） clientconfig（资源名称）</div>
+        </el-form-item>
+        <el-form-item label="hostPath">
+          <div class="tke-form-item_text">tz-config（卷名称） /etc/localtime（主机路径）不校验（路径检查类型）</div>
         </el-form-item>
       </el-form>
     </div>
 
     <div class="tke-card tke-formpanel-wrap mt10">
-      <h4  class="tke-formpanel-title">Kubernetes信息</h4>
-      <el-form  class="tke-form" label-position='left' label-width="150px" size="mini">
-        <el-form-item label="Kubernetes Labels">
-          <div class="tke-form-item_text">
-            beta.kubernetes.io/arch：amd64 <br>
-            beta.kubernetes.io/instance-type：QCLOUD<br>
-            beta.kubernetes.io/os：linux<br>
-            cloud.tencent.com/auto-scaling-group-id：asg-p3mt06kq<br>
-            cloud.tencent.com/node-instance-id：ins-5ic5rvb2<br>
-            failure-domain.beta.kubernetes.io/region：tpe<br>
-            failure-domain.beta.kubernetes.io/zone：390001<br>
-            kubernetes.io/hostname：10.0.128.8
-          </div>
+      <h4  class="tke-formpanel-title">容器（Containers）</h4>
+      <hr style="margin-left:-20px; margin-right:-20px">
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="kubedns" name="first">
+          <el-form  class="tke-form" label-position='left' label-width="120px" size="mini">
+            <el-form-item label="容器名称">
+              <div class="tke-form-item_text">kubedns</div>
+            </el-form-item>
+            <el-form-item label="镜像">
+              <div class="tke-form-item_text">tpeccr.ccs.tencentyun.com/library/kubedns-amd64:1.14.4</div>
+            </el-form-item>
+            <el-form-item label="CPU Requested">
+              <div class="tke-form-item_text">100m</div>
+            </el-form-item>
+            <el-form-item label="内存 Requested">
+              <div class="tke-form-item_text">30M</div>
+            </el-form-item>
+            <el-form-item label="运行参数">
+              <div class="tke-form-item_text">
+                --domain=cluster.local. <br>
+                --dns-port=10053<br>
+                --config-dir=/kube-dns-config<br>
+                --v=2
+              </div>
+            </el-form-item>
+            <el-form-item label="环境变量">
+              <div class="tke-form-item_text">PROMETHEUS_PORT=10055</div>
+            </el-form-item>
+            <el-form-item label="挂载点">
+              <div class="tke-form-item_text">
+                <span class="text-gray">数据卷名称:</span> kube-dns-config 
+                <span class="text-gray">目标路径:</span> /kube-dns-config 
+                <span class="text-gray">挂载子路径:</span> 未设置，默认全覆盖目标路径
+              </div>
+            </el-form-item>
+            <el-form-item label="存活检查">
+              <div class="tke-form-item_text">Detail</div>
+            </el-form-item>
+            <el-form-item label="就绪检查">
+              <div class="tke-form-item_text">Detail</div>
+            </el-form-item>
+
+          </el-form>
+        </el-tab-pane>
+        <el-tab-pane label="dnsmasq" name="second">dnsmasq</el-tab-pane>
+        <el-tab-pane label="sidecar" name="third">sidecar</el-tab-pane>
+      </el-tabs>
+      <!-- <el-form  class="tke-form" label-position='left' label-width="120px" size="mini">
+        <el-form-item label="configMap">
+          <div class="tke-form-item_text">kube-dns-config（卷名称） kube-dns（资源名称）</div>
         </el-form-item>
-        <el-form-item label="Kubernetes Annotations">
-          <div class="tke-form-item_text">
-            node.alpha.kubernetes.io/ttl：0 <br>
-            volumes.kubernetes.io/controller-managed-attach-detach：true
-          </div>
-        </el-form-item>
-        <el-form-item label="已分配资源">
-          <div class="tke-form-item_text">
-            <span class="text-gray">CPU:</span> 0.81 核，
-            <span class="text-gray">内存:</span> 0.55 GB
-          </div>
-        </el-form-item>
-        <el-form-item label="总可分配资源">
-          <div class="tke-form-item_text">
-            <span class="text-gray">CPU:</span> 0.94 核，
-            <span class="text-gray">内存:</span> 0.59 GB
-          </div>
-        </el-form-item>
-        <el-form-item label="容器运行时版本">
-          <div class="tke-form-item_text">docker://18.6.3</div>
-        </el-form-item>
-        <el-form-item label="Kubelet版本">
-          <div class="tke-form-item_text">v1.10.5-tke.6</div>
-        </el-form-item>
-        <el-form-item label="KubeProxy版本">
-          <div class="tke-form-item_text">v1.10.5-tke.6</div>
-        </el-form-item>
-      </el-form>
+      </el-form> -->
     </div>
    </div>
 </template>
@@ -93,7 +115,7 @@ export default {
   name: "deploymentDetailInfo",
   data() {
     return {
-      
+       activeName: 'first'
     };
   },
   components: {
@@ -104,7 +126,9 @@ export default {
    
   },
   methods: {
-   
+    handleClick(tab, event) {
+      console.log(tab, event);
+    }
   }
 };
 </script>
