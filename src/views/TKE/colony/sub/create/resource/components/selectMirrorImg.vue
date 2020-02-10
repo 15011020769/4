1600b9244f58de8158e97c7f5d972370f763793d
ji4396 <1187939346@qq.com>
2020-02-10 18:24:39 +0800

    TKE

--- src/views/TKE/colony/sub/create/resource/components/selectMirrorImg.vue
+++ src/views/TKE/colony/sub/create/resource/components/selectMirrorImg.vue
@@ -8,81 +8,52 @@
         </el-radio-group>
       </p>
       <el-tabs v-model="activeName" @tab-click="handleClick(activeName)">
-        <el-tab-pane label="我的镜像" name="first">
-        </el-tab-pane>
-        <el-tab-pane label="我的收藏" name="second">
-          
-        </el-tab-pane>
+        <el-tab-pane label="我的镜像" name="first"></el-tab-pane>
+        <el-tab-pane label="我的收藏" name="second"></el-tab-pane>
         <el-tab-pane label="公有镜像" name="third"></el-tab-pane>
         <el-tab-pane label="Docker Hub镜像" name="fourth"></el-tab-pane>
       </el-tabs>
       <div v-show="activeName=='first'||activeName=='third'">
-          <el-radio-group v-model="radio1">
-            <el-radio-button v-for="(v,i) in city" :key="i" :label="v"></el-radio-button>
-          </el-radio-group>
-          <p class="wdred">建议您选择与容器集群相同地域的镜像仓库，访问不同地域的镜像仓库受公网网络出入带宽影响。</p>
-          <el-input placeholder="请输入镜像名称" class="input-set">
-            <i slot="suffix" class="el-input__icon el-icon-search"></i>
-          </el-input>
-          <el-table  height="300" :data="tableData.slice((currpage - 1) * pagesize, currpage * pagesize)" tooltip-effect="dark" style="width: 100%">
-              <el-table-column width="55">
-                <template slot-scope="scope">
-                  <el-radio v-model="id" :label="scope.row.id">&nbsp;</el-radio>
-                </template>
-              </el-table-column>
-              <el-table-column label="名称" width="120">
-                <template slot-scope="scope">{{ scope.row.ID }}</template>
-              </el-table-column>
-              <el-table-column prop="name" label="类型" width="120">
-              </el-table-column>
-              <el-table-column prop="address" label="命名空间" >
-              </el-table-column>
-              <el-table-column prop="address" label="仓库地址" show-overflow-tooltip>
-              </el-table-column>
-            </el-table>
-            <!-- 分页 -->
-            <div class="pagstyle" style="height:70px;">
-              <span>共&nbsp;{{ TotalCount }}&nbsp;项</span>
-              <div class="pagestyle_right">
-                <el-pagination :page-size="pagesize" :pager-count="7" :page-sizes="[50, 40, 30, 20, 10]"
-                  layout="sizes, prev, pager, next" @current-change="handleCurrentChange"
-                  @size-change="handleSizeChange" :total="TotalCount"></el-pagination>
-              </div>
-            </div>
+        <el-radio-group v-model="radio1">
+          <el-radio-button v-for="(v,i) in city" :key="i" :label="v"></el-radio-button>
+        </el-radio-group>
+        <p class="wdred">建议您选择与容器集群相同地域的镜像仓库，访问不同地域的镜像仓库受公网网络出入带宽影响。</p>
+        <el-input placeholder="请输入镜像名称" class="input-set">
+          <i slot="suffix" class="el-input__icon el-icon-search"></i>
+        </el-input>
       </div>
       <div v-show="activeName=='second'||activeName=='fourth'">
-          <el-input placeholder="请输入镜像名称" class="input-set">
-            <i slot="suffix" class="el-input__icon el-icon-search"></i>
-          </el-input>
-     </div>
-        <div>  
-          <el-table  height="300" :data="tableData.slice((currpage - 1) * pagesize, currpage * pagesize)" tooltip-effect="dark" style="width: 100%">
-              <el-table-column width="55">
-                <template slot-scope="scope">
-                  <el-radio v-model="id" :label="scope.row.id">&nbsp;</el-radio>
-                </template>
-              </el-table-column>
-              <el-table-column label="名称" width="120">
-                <template slot-scope="scope">{{ scope.row.ID }}</template>
-              </el-table-column>
-              <el-table-column prop="name" label="类型" width="120">
-              </el-table-column>
-              <el-table-column prop="address" label="命名空间" >
-              </el-table-column>
-              <el-table-column prop="address" label="仓库地址" show-overflow-tooltip>
-              </el-table-column>
-            </el-table>
-            <!-- 分页 -->
-            <div class="pagstyle" style="height:70px;">
-              <span>共&nbsp;{{ TotalCount }}&nbsp;项</span>
-              <div class="pagestyle_right">
-                <el-pagination :page-size="pagesize" :pager-count="7" :page-sizes="[50, 40, 30, 20, 10]"
-                  layout="sizes, prev, pager, next" @current-change="handleCurrentChange"
-                  @size-change="handleSizeChange" :total="TotalCount"></el-pagination>
-              </div>
-            </div>
+        <el-input placeholder="请输入镜像名称" class="input-set">
+          <i slot="suffix" class="el-input__icon el-icon-search"></i>
+        </el-input>
       </div>
-    </div>
+        <el-table height="300" :data="tableData.slice((currpage - 1) * pagesize, currpage * pagesize)"
+          tooltip-effect="dark" style="width: 100%">
+          <el-table-column width="55">
+            <template slot-scope="scope">
+              <el-radio v-model="id" :label="scope.row.id">&nbsp;</el-radio>
+            </template>
+          </el-table-column>
+          <el-table-column label="名称" width="120">
+            <template slot-scope="scope">{{ scope.row.ID }}</template>
+          </el-table-column>
+          <el-table-column prop="name" label="类型" width="120">
+          </el-table-column>
+          <el-table-column prop="address" label="命名空间">
+          </el-table-column>
+          <el-table-column prop="address" label="仓库地址" show-overflow-tooltip>
+          </el-table-column>
+        </el-table>
+        <!-- 分页 -->
+        <div class="pagstyle" style="height:70px;">
+          <span>共&nbsp;{{ TotalCount }}&nbsp;项</span>
+          <div class="pagestyle_right">
+            <el-pagination :page-size="pagesize" :pager-count="7" :page-sizes="[50, 40, 30, 20, 10]"
+              layout="sizes, prev, pager, next" @current-change="handleCurrentChange" @size-change="handleSizeChange"
+              :total="TotalCount"></el-pagination>
+          </div>
+        </div>
+      </div>
     <span slot="footer" class="dialog-footer">
       <el-button @click="cancle">取 消</el-button>
       <el-button type="primary" @click="sure">确 定</el-button>
@@ -131,13 +102,13 @@
         this.$emit('close', this.visible)
       },
       handleClick(tab) {
-       this.activeName=tab;
+        this.activeName = tab;
       },
       // 改变页数
       handleCurrentChange(val) {
         this.currpage = val;
       },
-       handleSizeChange(val) {
+      handleSizeChange(val) {
         console.log(`每頁 ${val} 條`);
       },
     },
@@ -146,7 +117,7 @@
 </script>
 <style scoped lang="scss">
   /* css */
- 
+
   .dialog>>>.el-dialog {
     height: 100%;
     margin-top: 0px !important;
@@ -157,9 +128,10 @@
     right: 0px;
     overflow: auto;
   }
-  .dialog>>>.el-dialog__footer{
-         text-align: center !important;   
-        }
+
+  .dialog>>>.el-dialog__footer {
+    text-align: center !important;
+  }
 
   .wdred {
     color: red;
@@ -170,10 +142,12 @@
     background: #f2f2f2;
     padding: 10px;
   }
-  .input-set>>>input{
-      margin: 15px 0px;
-      height: 30px;
+
+  .input-set>>>input {
+    margin: 15px 0px;
+    height: 30px;
   }
+
   .pagstyle {
     display: flex;
     justify-content: space-between;
@@ -190,5 +164,5 @@
       }
     }
   }
-       
+
 </style>
