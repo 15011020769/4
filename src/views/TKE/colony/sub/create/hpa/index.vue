c8bdb28c029fe96158d44073091b0e2b811e3a46
wangyuren <997959694@qq.com>
2020-02-10 19:51:15 +0800

    新增集群日志和自动伸缩新建

--- src/views/TKE/colony/sub/create/hpa/index.vue
+++ src/views/TKE/colony/sub/create/hpa/index.vue
@@ -19,10 +19,105 @@
       <div class="tke-card tke-formpanel-wrap mb60">
         <el-form  class="tke-form" :model="hpa" label-position='left' label-width="120px" size="mini">
           <el-form-item label="名称">
-            <el-input class="w200" v-model="hpa.name" placeholder="请输入名称"></el-input>
+            <el-input class="w180" v-model="hpa.name" placeholder="请输入名称"></el-input>
             <p>最长63个字符，只能包含小写字母、数字及分隔符("-")，且必须以小写字母开头，数字或小写字母结尾</p>
           </el-form-item>
           
+           <el-form-item label="命名空间">
+             <el-select v-model="hpa.value1" placeholder="请选择" size="mini">
+                <el-option
+                  v-for="item in hpa.option1"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+          </el-form-item>
+
+          <el-form-item label="工作负载类型">
+             <el-select v-model="hpa.value2" placeholder="请选择" size="mini">
+                <el-option
+                  v-for="item in hpa.option2"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+          </el-form-item>
+
+          <el-form-item label="关联工作负载">
+             <el-select v-model="hpa.value3" placeholder="请选择" size="mini">
+                <el-option
+                  v-for="item in hpa.option3"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+          </el-form-item>
+
+           <el-form-item label="解决策略">
+              <div class='app-tke-fe-form w520'>
+                <div class="form-input"
+                 v-for="(domain, index) in hpa.domains"
+                :key="domain.key"
+                :prop="'domains.' + index + '.value'">
+                     <el-select v-model="domain.value4" placeholder="请选择" size="mini" class="w100">
+                      <el-option
+                        v-for="item in domain.options"
+                        :key="item.label"
+                        :label="item.label"
+                        :value="item.value">
+                      </el-option>
+                    </el-select>
+
+                     <el-select v-model="domain.value5" placeholder="请选择" size="mini" class="w200">
+                      <el-option
+                        v-for="item in domain.option1"
+                        :key="item.label"
+                        :label="item.label"
+                        :value="item.value"
+                        v-if='domain.value4 == 1'>
+                      </el-option>
+                      <el-option
+                        v-for="item in domain.option2"
+                        :key="item.label"
+                        :label="item.label"
+                        :value="item.value"
+                        v-if='domain.value4 == 2'>
+                      </el-option>
+                      <el-option
+                        v-for="item in domain.option3"
+                        :key="item.label"
+                        :label="item.label"
+                        :value="item.value"
+                        v-if='domain.value4 == 3'>
+                      </el-option>
+                      <el-option
+                        v-for="item in domain.option4"
+                        :key="item.label"
+                        :label="item.label"
+                        :value="item.value"
+                        v-if='domain.value4 == 4'>
+                      </el-option>
+                    </el-select>
+
+                    <el-input v-model="domain.valueKey" size="mini" placeholder="变量值" class='w100'></el-input><span v-if='domain.value4 == 1'>核</span><span v-if='domain.value4 == 2'>MiB</span><span v-if='domain.value4 == 3'>KB/s</span><span v-if='domain.value4 == 4'>Mbps</span>
+
+                    <el-tooltip class="item" effect="light" content="至少保留一个指标" placement="right">
+                        <i class="el-icon-close" @click.prevent="removeDomain(hpa.domains,index)"></i>
+                    </el-tooltip>
+
+                </div>
+                <el-link type="primary" style="cursor: pointer;font-size:12px;"  @click="addDomain">新增指标</el-link>
+              </div>
+          </el-form-item>
+          <el-form-item label="实例范围">
+                <el-input class='w100' v-model='hpa.vLeft'></el-input>
+                <span class='w-span'>~</span>  
+                <el-input class='w100' v-model='hpa.vRight'></el-input>
+                <p>在设定的实例范围内自动调节，不会超出该设定范围</p>
+          </el-form-item>
         </el-form>
 
        
@@ -48,7 +143,123 @@ export default {
     return {
       hpa: {
         name: '',
-       
+        option1:[
+          {
+            value: 'kube-system',
+            label: 'kube-system'
+          }
+        ],
+        option2:[
+          {
+            value: 'defualt',
+            label: 'defualt'
+          }
+        ],
+        option3:[
+          {
+            value: 'kube',
+            label: 'kube'
+          }
+        ],
+        domains:[
+          {
+            options:[{
+               value: 1,
+               label: 'CPU'
+            },{
+               value: 2,
+               label: '内存'
+            },{
+               value: 3,
+               label: '硬盘'
+            },{
+               value: 4,
+               label: '网络'
+            }],
+
+            option1:[{
+               value: 1,
+               label: 'CPU使用量'
+            },{
+               value: 2,
+               label: 'CPU利用率(占节点)'
+            },{
+               value: 3,
+               label: 'CPU利用率(占Request)'
+            },{
+               value: 4,
+               label: 'CPU利用率(占Limit)'
+            }],
+
+            option2:[{
+               value: 1,
+               label: '内存使用量'
+            },{
+               value: 1,
+               label: '内存使用量(不含Cache)'
+            },{
+               value: 2,
+               label: '内存使用量(占节点)'
+            },{
+               value: 2,
+               label: '内存使用量(占节点、不含Cache)'
+            },{
+               value: 3,
+               label: '内存使用量(占Request)'
+            },{
+               value: 3,
+               label: '内存使用量(占Request、不含Cache)'
+            },{
+               value: 4,
+               label: '内存使用量(占Limit)'
+            },{
+               value: 4,
+               label: '内存使用量(占Limit、不含Cache)'
+            }],
+
+            option3:[{
+               value: 1,
+               label: '硬盘写流量'
+            },{
+               value: 2,
+               label: '硬盘读流量'
+            },{
+               value: 3,
+               label: '硬盘读IOPS'
+            },{
+               value: 4,
+               label: '硬盘写IOPS'
+            }],
+
+            option4:[{
+               value: 1,
+               label: '网络出带宽'
+            },{
+               value: 2,
+               label: '网络入带宽'
+            },{
+               value: 3,
+               label: '网络出流量'
+            },{
+               value: 4,
+               label: '网络入流量'
+            },{
+               value: 3,
+               label: '网络出包量'
+            },{
+               value: 4,
+               label: '网络入包量'
+            }],
+            value4:1,
+            value5:1,
+            valueKey:''
+          }
+        ],
+        value1:'',
+        value2:'',
+        value3:'',
+        vLeft:'',
+        vRight:''
       }  
     };
   },
@@ -62,13 +273,135 @@ export default {
   methods: {
     //返回上一层
     goBack(){
-          this.$router.go(-1);
+        this.$router.go(-1);
     },
+    removeDomain (domain,index) {
+      // console.log(domain,index)
+      //  var index = this.form.domains.indexOf(item)
+        if (domain.length !== 1) {
+            this.hpa.domains.splice(index, 1)
+        }
+    },
+    addDomain () {
+        this.hpa.domains.push({
+            options:[{
+               value: 1,
+               label: 'CPU'
+            },{
+               value: 2,
+               label: '内存'
+            },{
+               value: 3,
+               label: '硬盘'
+            },{
+               value: 4,
+               label: '网络'
+            }],
+
+            option1:[{
+               value: 1,
+               label: 'CPU使用量'
+            },{
+               value: 2,
+               label: 'CPU利用率(占节点)'
+            },{
+               value: 3,
+               label: 'CPU利用率(占Request)'
+            },{
+               value: 4,
+               label: 'CPU利用率(占Limit)'
+            }],
+
+            option2:[{
+               value: 1,
+               label: '内存使用量'
+            },{
+               value: 1,
+               label: '内存使用量(不含Cache)'
+            },{
+               value: 2,
+               label: '内存使用量(占节点)'
+            },{
+               value: 2,
+               label: '内存使用量(占节点、不含Cache)'
+            },{
+               value: 3,
+               label: '内存使用量(占Request)'
+            },{
+               value: 3,
+               label: '内存使用量(占Request、不含Cache)'
+            },{
+               value: 4,
+               label: '内存使用量(占Limit)'
+            },{
+               value: 4,
+               label: '内存使用量(占Limit、不含Cache)'
+            }],
+
+            option3:[{
+               value: 1,
+               label: '硬盘写流量'
+            },{
+               value: 2,
+               label: '硬盘读流量'
+            },{
+               value: 3,
+               label: '硬盘读IOPS'
+            },{
+               value: 4,
+               label: '硬盘写IOPS'
+            }],
+
+            option4:[{
+               value: 1,
+               label: '网络出带宽'
+            },{
+               value: 2,
+               label: '网络入带宽'
+            },{
+               value: 3,
+               label: '网络出流量'
+            },{
+               value: 4,
+               label: '网络入流量'
+            },{
+               value: 3,
+               label: '网络出包量'
+            },{
+               value: 4,
+               label: '网络入包量'
+            }],
+            value4:1,
+            value5:1,
+            valueKey:''
+          }
+        )
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.app-tke-fe-form{
+    background-color: #f2f2f2;
+    padding: 10px;
+}
+.form-input{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top:10px;
+    span{
+        margin:0 10px;
+    }
+    i{
+        margin:0 10px;
+        cursor: pointer;
+    }
+}
+.w-span{
+  margin:0 10px;
+}
 
 </style>
 
--- src/views/TKE/colony/sub/list/log/index.vue
+++ src/views/TKE/colony/sub/list/log/index.vue
@@ -1,7 +1,81 @@
  <!-- 日志 -->
 <template>
   <div >
-    <p>日志</p>
+    <h2>日志</h2>
+    <el-card class="box-card">
+      <h3 style="margin-bottom:20px;">条件筛选</h3>
+      <el-form class="tke-form">
+        <el-form-item label="工作负载选项">
+           <el-tooltip class="item" effect="light" content="工作负载类型、命名空间、Workload实例" placement="right">
+              <i class="el-icon-info"></i>
+            </el-tooltip>
+               <el-select v-model="value1" placeholder="请选择" size="mini" class="ml10">
+                <el-option
+                  v-for="item in option1"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+              <el-select v-model="value2" placeholder="请选择" size="mini" class="ml10" >
+                <el-option
+                  v-for="item in option2"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+               <el-select v-model="value3" placeholder="请选择" size="mini" class="ml10" :disabled='value2==1?true:false'>
+                <el-option
+                  v-for="item in option3"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+        </el-form-item>
+         <el-form-item label="Pod选项">
+           <el-tooltip class="item" effect="light" content="Pod实例、Container实例" placement="right">
+              <i class="el-icon-info"></i>
+            </el-tooltip>
+               <el-select v-model="value4" placeholder="请选择" size="mini" class="ml10">
+                <el-option
+                  v-for="item in option4"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+              <el-select v-model="value5" placeholder="请选择" size="mini" class="ml10" >
+                <el-option
+                  v-for="item in option5"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+        </el-form-item>
+        <el-form-item label='其他选项'>
+            <span >自动刷新</span>
+            <el-switch
+            v-model="flag"
+            class='ml10'
+            >
+          </el-switch>
+          <el-select v-model="value6" placeholder="请选择" size="mini" class="ml10" >
+                <el-option
+                  v-for="item in option6"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+            </el-select>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <el-card class='box-card'> 
+      <div class='box-black'>1</div>
+    </el-card>
   </div>
 </template>
 
@@ -15,8 +89,48 @@ export default {
   name: "create",
   data() {
     return {
-    
-    };
+     option1: [{
+          value: 'tfy-pub',
+          label: 'tfy-pub'
+        }, {
+          value: 'default',
+          label: 'default'
+        }, {
+          value: 'kube-public',
+          label: 'kube-public'
+        }, {
+          value: 'kube-system',
+          label: 'kube-system'
+        }],
+      option2: [{
+          value: 1,
+          label: '请选择工作负载类型'
+        }, {
+          value: 'Deployment',
+          label: 'Deployment'
+        }, {
+          value: 'StatefulSet',
+          label: 'StatefulSet'
+        }, {
+          value: 'DaemonSet',
+          label: 'DaemonSet'
+        },
+        {
+          value: 'Job',
+          label: 'Job'
+        }],
+        option3:[],
+        option4:[],
+        option5:[],
+        option6:[],
+        value1: '',
+        value2: '',
+        value3: '',
+        value4: '',
+        value5: '',
+        value6: '',
+        flag:false
+    }
   },
   components: {
     HeadCom,
@@ -34,8 +148,17 @@ export default {
 };
 </script>
 
-<style lang="scss" tke-formpanel>
-
-
+<style lang="scss" scoped>
+  .box-card {
+    width: 100%;
+    margin:20px 0;
+  }
+  .item {
+      margin: 4px;
+    }
+  .box-black{
+    margin:10px;
+    background:black;
+  }
 </style>
 
