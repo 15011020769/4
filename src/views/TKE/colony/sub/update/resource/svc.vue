214a5cf1f517f924d52437e7d1c0ef4b1239e945
ji4396 <1187939346@qq.com>
2020-02-14 14:43:13 +0800

    tke

---
+++ src/views/TKE/colony/sub/update/resource/svc.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="" id="">
+
+    </div>
+</template>
+<script>
+export default {
+   data() {
+      return {
+
+      }
+   },
+   mounted() {
+
+   },
+   methods:{
+
+   },
+}
+</script>
+<style scoped>
+/* css */
+</style>
