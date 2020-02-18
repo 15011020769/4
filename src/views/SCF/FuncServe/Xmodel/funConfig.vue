a4f362025577eb23dba3f2f814ce34d12ed892b9
zhaobobin <254787741@qq.com>
2020-02-19 03:55:38 +0800

    云函数---函数信息

--- src/views/SCF/FuncServe/Xmodel/funConfig.vue
+++ src/views/SCF/FuncServe/Xmodel/funConfig.vue
@@ -43,13 +43,17 @@
       </div>
       <div class="Content" v-if="!Congigload">
         <p>所属网络</p>
-        <p v-if="VpcConfig.VpcId===''">无VPC</p>
-        <p v-else>{{ConfigDate.VpcConfig.VpcId}}</p>
+        <p v-if="Vpc_Smb.Vpc===''">无VPC</p>
+        <p v-else><a href=""> {{ConfigDate.VpcConfig.VpcId}}</a>
+          ({{Vpc_Smb.Vpc.VpcId}} | {{Vpc_Smb.Vpc.VpcName}} | {{Vpc_Smb.Vpc.CidrBlock}})
+        </p>
       </div>
       <div class="Content" v-if="!Congigload">
         <p>所属子网</p>
-        <p v-if="VpcConfig.SubnetId===''">无子网</p>
-        <p v-else>{{ConfigDate.VpcConfig.SubnetId}}</p>
+        <p v-if="Vpc_Smb.Smb===''">无子网</p>
+        <p v-else><a href=""> {{ConfigDate.VpcConfig.SubnetId}}</a>
+          ({{Vpc_Smb.Smb.SubnetId}} | {{Vpc_Smb.Smb.SubnetName}} | {{Vpc_Smb.Smb.CidrBlock}})
+        </p>
       </div>
     </div>
     <div v-if="edit===true">
@@ -61,7 +65,7 @@
         <p>运行角色</p>
         <div>
           <el-select v-model="RoleValue" placeholder="请选择">
-            <el-option v-for="item in RoleArr" :key="item.value" :label="item.label" :value="item.value">
+            <el-option v-for="item in RoleArr" :key="item.RoleName" :label="item.RoleName" :value="item.RoleName">
             </el-option>
           </el-select>
         </div>
@@ -146,11 +150,13 @@
         <p></p>
         <div>
           <el-select v-model="VPCvalue" placeholder="请选择">
-            <el-option v-for="item in VPCoptions" :key="item.value" :label="item.label" :value="item.value">
+            <el-option v-for="item in VPCoptions" :key="item.VpcId"
+              :label="item.VpcId+' | '+ item.VpcName+' | '+item.CidrBlock" :value="item.VpcId">
             </el-option>
           </el-select>
           <el-select v-model="Subvalue" placeholder="请选择">
-            <el-option v-for="item in Suboptions" :key="item.value" :label="item.label" :value="item.value">
+            <el-option v-for="item in Suboptions" :key="item.SubnetId"
+              :label="item.SubnetId+' | '+ item.SubnetName+' | '+item.CidrBlock" :value="item.SubnetId">
             </el-option>
           </el-select>
           <p style="color:#888">如现有的网络不合适，您可以去控制台
@@ -159,8 +165,10 @@
             <a href="">新建子网</a> </p>
         </div>
       </div>
-
-
+      <div class="Preservation">
+        <el-button type="primary" size="small" @click="_Preservation">保存</el-button>
+        <el-button size="small" @click="_cancel"> 取消</el-button>
+      </div>
 
 
     </div>
@@ -169,6 +177,10 @@
 <script>
   import {
     SCF_DETAILS,
+    VPCS_LIST,
+    SUBNET_LIST,
+    DESCRIB_ROLE,
+    UPD_CONFIG
   } from "@/constants";
   import {
     ErrorTips
@@ -182,6 +194,10 @@
         ConfigDate: '',
         Variables: [],
         VpcConfig: {},
+        Vpc_Smb: {
+          Vpc: {},
+          Smb: {}
+        },
         RoleValue: '', //运行角色
         RoleArr: [], //运行角色数组
         MemoryValue: '', //内存
@@ -251,10 +267,21 @@
     },
     mounted() {
       this.GetDate()
+      this.AddScience()
+      this._Getrole()
+    },
+    watch: {
+      VpcConfig() {
+        this._GetVpc()
+      },
+      VPCvalue() {
+        this._GetSmb()
+      }
     },
     methods: {
       //获取详情数据
       GetDate() {
+        this.Congigload = true
         let param = {
           Region: localStorage.getItem('regionv2'),
           Version: "2018-04-16",
@@ -264,17 +291,13 @@
           if (res.Response.Error === undefined) {
             this.ConfigDate = res.Response
             this.Variables = res.Response.Environment.Variables
-            if (this.Variables.length === 0) {
-              this.MemoryArr = [{}]
-            } else {
-              this.MemoryArr = this.Variables
+            if (this.Variables.length !== 0) {
+              this.ScienceArr = this.Variables
             }
             this.VpcConfig = res.Response.VpcConfig
             this.RoleValue = this.ConfigDate.Role
             this.MemoryValue = this.ConfigDate.MemorySize
             this.TimeoutValue = this.ConfigDate.Timeout
-
-            this.Congigload = false
           } else {
             let ErrTips = {
               'InternalError': '内部错误',
@@ -287,6 +310,49 @@
               'UnauthorizedOperation': '未授权操作',
               'UnauthorizedOperation.CAM': 'CAM鉴权失败',
               'UnauthorizedOperation.CodeSecret': '无访问代码权限'
+            };
+            let ErrOr = Object.assign(ErrorTips, ErrTips);
+            this.$message({
+              message: ErrOr[res.Response.Error.Code],
+              type: "error",
+              showClose: true,
+              duration: 0
+            });
+          }
+        }).then(() => {
+          this._GetVpc()
+        })
+      },
+      _Getrole() { //获取角色
+        let param = {
+          Region: localStorage.getItem('regionv2'),
+          Version: "2019-01-16",
+          Page: 1,
+          Rp: 200
+        };
+        this.axios.post(DESCRIB_ROLE, param).then(res => {
+          if (res.Response.Error === undefined) {
+            let List = res.Response.List
+            List.forEach(item => {
+              let strategyArr = JSON.parse(item.PolicyDocument)
+              let strategy = strategyArr.statement[0].principal.service
+              if (typeof strategy === 'object') {
+                strategy.forEach(i => {
+                  if (i.split(".")[0] === 'scf') {
+                    this.RoleArr.push(item)
+                  }
+                });
+              } else if (typeof strategy === "string") {
+                if (strategy.split(".")[0] === 'scf') {
+                  this.RoleArr.push(item)
+                }
+              }
+            });
+            console.log(this.RoleArr)
+          } else {
+            let ErrTips = {
+              'InternalError.SystemError': 'InternalError.SystemError',
+              'InvalidParameter.ParamError': '非法入参'
             };
             let ErrOr = Object.assign(ErrorTips, ErrTips);
             this.$message({
@@ -316,6 +382,153 @@
           this.closeshow = false;
         }
       },
+      //查询私有网络列表
+      _GetVpc() {
+        let param = {
+          Region: localStorage.getItem('regionv2'),
+          Version: "2017-03-12",
+        };
+        this.axios.post(VPCS_LIST, param).then(res => {
+          if (res.Response.Error === undefined) {
+            this.VPCoptions = res.Response.VpcSet
+            if (this.VpcConfig.VpcId === '') {
+              this.Vpc_Smb.Vpc = ''
+            } else {
+              this.VPCoptions.forEach(item => {
+                if (this.VpcConfig.VpcId === item.VpcId) {
+                  this.Vpc_Smb.Vpc = item
+                  this.VPCvalue = this.Vpc_Smb.Vpc.VpcId
+                }
+              });
+            }
+          } else {
+            let ErrTips = {
+              'InvalidParameter.Coexist': '参数不支持同时指定',
+              'InvalidParameterValue.Malformed': '入参格式不合法',
+              'ResourceNotFound': '资源不存在'
+            };
+            let ErrOr = Object.assign(ErrorTips, ErrTips);
+            this.$message({
+              message: ErrOr[res.Response.Error.Code],
+              type: "error",
+              showClose: true,
+              duration: 0
+            });
+          }
+        }).then(() => {
+          this._GetSmb()
+        })
+      },
+      //查询子网列表
+      _GetSmb() {
+        this.Subvalue = ''
+        this.Suboptions = []
+        let param = {
+          Region: localStorage.getItem('regionv2'),
+          Version: "2017-03-12",
+          'Filters.0.Name': 'vpc-id',
+          'Filters.0.Values.0': this.VPCvalue
+        };
+        this.axios.post(SUBNET_LIST, param).then(res => {
+          if (res.Response.Error === undefined) {
+            this.Suboptions = res.Response.SubnetSet
+            if (this.VpcConfig.SubnetId === '') {
+              this.Vpc_Smb.Smb = ''
+            } else {
+              this.Suboptions.forEach(item => {
+                if (this.VpcConfig.SubnetId === item.SubnetId) {
+                  this.Vpc_Smb.Smb = item
+                  this.Subvalue = this.Vpc_Smb.Smb.SubnetId
+                }
+              });
+            }
+            this.Congigload = false
+          } else {
+            let ErrTips = {
+              'InvalidParameter.Coexist': '参数不支持同时指定',
+              'InvalidParameterValue.Malformed': '入参格式不合法',
+              'ResourceNotFound': '资源不存在'
+            };
+            let ErrOr = Object.assign(ErrorTips, ErrTips);
+            this.$message({
+              message: ErrOr[res.Response.Error.Code],
+              type: "error",
+              showClose: true,
+              duration: 0
+            });
+          }
+        });
+      },
+      //保存更改配置
+      _Preservation() {
+        let param = {
+          Region: localStorage.getItem('regionv2'),
+          Version: "2018-04-16",
+          FunctionName: this.ConfigDate.FunctionName,
+          Description: this.ConfigDate.Description,
+          MemorySize: this.MemoryValue,
+          Timeout: this.TimeoutValue
+        };
+        if (this.RoleValue !== '') {
+          param['Role'] = this.RoleValue
+        }
+        if (this.IntranetValue === true) {
+          param['VpcConfig.VpcId'] = this.VPCvalue
+          param['VpcConfig.SubnetId'] = this.Subvalue
+        }
+        for (let i in this.ScienceArr) {
+          (param["Environment.Variables." + i + ".Key"] = this.ScienceArr[
+            i
+          ].Key),
+          (param["Environment.Variables." + i + ".Value"] = this.ScienceArr[
+            i
+          ].Value);
+        }
+        this.axios.post(UPD_CONFIG, param).then(res => {
+          if (res.Response.Error === undefined) {
+            this.$message({
+              message: '修改成功',
+              type: "success",
+              showClose: true,
+              duration: 0
+            });
+            this.edit = false
+            this.GetDate()
+          } else {
+            let ErrTips = {
+              'InternalError.System': '内部系统错误',
+              'InvalidParameter.Payload': '请求参数不合法',
+              'InvalidParameterValue': '参数取值错误',
+              'InvalidParameterValue.Cls': 'Cls传入错误',
+              'InvalidParameterValue.Description': 'Description传入错误',
+              'InvalidParameterValue.EipConfig': 'EipConfig参数错误',
+              'InvalidParameterValue.Environment': 'Environment传入错误',
+              'InvalidParameterValue.FunctionName': '函数不存在',
+              'InvalidParameterValue.Handler': 'Handler传入错误',
+              'InvalidParameterValue.Layers': 'Layers参数传入错误',
+              'InvalidParameterValue.MemorySize': 'MemorySize错误',
+              'LimitExceeded.Memory': '内存超出最大限制',
+              'LimitExceeded.Timeout': 'Timeout超出最大限制',
+              'ResourceNotFound.Function': '函数不存在',
+              'ResourceNotFound.FunctionName': '函数不存在',
+              'ResourceNotFound.Layer': 'layer不存在',
+              'ResourceNotFound.Vpc': 'VPC或子网不存在',
+              'UnauthorizedOperation.CAM': 'CAM鉴权失败'
+            };
+            let ErrOr = Object.assign(ErrorTips, ErrTips);
+            this.$message({
+              message: ErrOr[res.Response.Error.Code],
+              type: "error",
+              showClose: true,
+              duration: 0
+            });
+          }
+        });
+      },
+      //取消
+      _cancel() {
+        this.edit = false
+      }
     }
   }
 
@@ -384,6 +597,13 @@
           border-bottom: none;
         }
       }
+    }
+
+
+    .Preservation {
+      margin-top: 20px;
+      padding-top: 20px;
+      border-top: 1px solid #ddd;
     }
   }
 
