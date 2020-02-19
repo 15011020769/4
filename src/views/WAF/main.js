73a4eeaa148e9e436889b43721275e23ae1eaccb
iSayid <sayid>
2020-02-19 13:48:49 +0800

    增加WAF通用响应处理函数

--- src/views/WAF/main.js
+++ src/views/WAF/main.js
@@ -11,6 +11,8 @@ import '@/assets/iconfont/iconfont.css'
 import filters from '@/filters/filters.js'
 import echarts from 'echarts'
 import i18n from './language/i18n.js' // 引入i18n包
+import { ErrorTips } from "@/components/ErrorTips"
+
 // 引入基本模板
 // const echarts = require('echarts/lib/echarts')
 // require("echarts/lib/component/legend")
@@ -19,6 +21,42 @@ import i18n from './language/i18n.js' // 引入i18n包
     // 引入提示框和title组件，图例
 // require('echarts/lib/component/tooltip')
 Vue.prototype.$echarts = echarts
+/**
+ * 通用响应处理函数
+ * @param {Object} resp 响应对象
+ * @param {Function} successCallback 成功回调
+ * @param {Object} cusError 错误信息对象
+ * @param {Function} failedCallback 失败回调
+ * @param {String} successMsg 成功提示信息
+ */
+Vue.prototype.generalRespHandler = function(
+  resp,
+  successCallback,
+  cusError={},
+  failedCallback=() => {},
+  successMsg='',
+) {
+  if (resp.Response.Error) {
+    failedCallback()
+    let ErrOr = Object.assign(ErrorTips, cusError)
+    this.$message({
+      message: ErrOr[resp.Response.Error.Code],
+      type: 'error',
+      showClose: true,
+      duration: 0
+    })
+  } else {
+    successCallback()
+    if (successMsg) {
+      this.$message({
+        message: successMsg,
+        type: 'success',
+        showClose: true,
+        duration: 0
+      })
+    }
+  }
+}
 for (let key in filters) {
     Vue.filter(key, filters[key])
 }
