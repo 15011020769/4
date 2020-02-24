67392227ebb6edae0f6f0877d061182e67ca967c
zhaobobin <254787741@qq.com>
2020-02-24 18:12:46 +0800

    云监控bug

--- src/views/CM/VPNchannel/index.vue
+++ src/views/CM/VPNchannel/index.vue
@@ -42,8 +42,8 @@
         </el-table-column>
         <el-table-column prop :label="$t('CVM.vpnwg')">
           <template slot-scope="scope">
-            <a
-              :href="'../VPC/index.html#/vpnGateway/vpnGatewayDetial/'+ scope.row.VpnGatewayId + '/ap-taipei'">{{scope.row.VpnGatewayId}}</a>
+            <a :href="'../VPC/index.html#/vpnGateway/vpnGatewayDetial/'+  scope.row.VpcId +scope.row.VpnGatewayId +
+              '/ap-taipei/1'">{{scope.row.VpnGatewayId}}</a>
             <p>{{scope.row.vpnGwName}}</p>
           </template>
         </el-table-column>
