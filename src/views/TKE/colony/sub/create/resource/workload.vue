8809b001fd67a2e468f88e8af874c876bc164027
任茂勇 <81314306@qq.com>
2020-02-26 22:20:30 +0800

    deployment

--- src/views/TKE/colony/sub/create/resource/workload.vue
+++ src/views/TKE/colony/sub/create/resource/workload.vue
@@ -18,7 +18,7 @@
       <div class="tke-card tke-formpanel-wrap mb60">
         <el-form class="tke-form special" :model="wl" :rules="rules" ref="wl" label-position="left" label-width="120px" size="mini">
           <el-form-item label="名称" prop="name">
-            <el-input class="w200" v-model="wl.name" placeholder="请输入Workload名称"></el-input>
+            <el-input class="w200" v-model="wl.name" placeholder="请输入Workload名称" @blur="addLabel(e)"></el-input>
             <p>
               最长40个字符，只能包含小写字母、数字及分隔符("-")，且必须以小写字母开头，数字或小写字母结尾
             </p>
@@ -32,8 +32,8 @@
             <div v-for="(v,i) in wl.labels" :key="i">
              <el-input :disabled="v.key=='k8s-app'" class="w100" v-model="v.key"></el-input> = <el-input class="w100"  v-model="v.value" prop="labels"></el-input>
               <el-tooltip class="item" effect="light" content="默认标签不可删除" placement="right" v-if="v.key=='k8s-app'">
-                   <i class="el-icon-close"  style="font-size:20px;margin-left:20px;cursor:pointer"></i>
-                </el-tooltip>
+                <i class="el-icon-close"  style="font-size:20px;margin-left:20px;cursor:pointer"></i>
+              </el-tooltip>
               <i class="el-icon-close"  style="font-size:20px;margin-left:20px;cursor:pointer" v-else @click="delAddVar(i)"></i>
             </div>
              <span class="disable" v-if="addVarFlag">新增变量</span>
@@ -166,6 +166,15 @@
                     <p v-show="v.mirrorPullTactics=='IfNotPresent'">默认使用本地镜像，若本地无该镜像则远程拉取该镜像</p>
                     <p v-show="v.mirrorPullTactics=='Never'">只使用本地镜像，若本地没有该镜像将报异常</p>
                   </template>
+                </el-form-item>
+                <el-form-item label="挂载点" v-show="wl.dataJuan.length > 0">
+                  <div v-show="showMountPoint" v-for="(i,point) in wl.pointList" :key="i">
+                    <el-select v-model="wl.pointName"></el-select>
+                    <el-input v-model="wl.mountPath"></el-input>
+                    <el-input v-model="wl.subPath"></el-input>
+                    <el-select></el-select>
+                  </div>
+                  <el-button type="text" @click="changIsShowmount()">添加挂载点</el-button>
                 </el-form-item>
                 <el-form-item label="CPU/内存限制">
                   <div class="cpu-limit">
@@ -610,8 +619,139 @@
           </div>
           <!--  </el-form> -->
         </el-form>
-         <el-form  class="tke-form" :model="svc" label-position='left' label-width="120px" size="mini">
-            <Service></Service>
+        <el-form  class="tke-form" :model="wl" label-position='left' label-width="120px" size="mini">
+            <!-- <Service></Service> -->
+          <div class="card">
+            <h3 style="margin-bottom:11px;">访问设置(Service)</h3>
+            <el-form-item label="Service">
+              <el-checkbox v-model="wl.serviceEnbel" @change="changEnbel()">启用</el-checkbox> 
+            </el-form-item>
+            <div v-if="showServiceModal">
+              <el-form-item label="服务访问方式">
+                <el-radio v-model="wl.radio" label="1">提供公网访问</el-radio>
+                <el-radio v-model="wl.radio" label="2">仅在集群内访问</el-radio>
+                <el-radio v-model="wl.radio" label="3">VPC内网访问</el-radio>
+                <el-radio v-model="wl.radio" label="4">主机端口访问</el-radio>
+                <a href="" style="padding-left:10px;">如何选择</a><i class="el-icon-edit-outline"></i>
+                <!-- 方式介绍 -->
+                <div>
+                  <div v-if="wl.radio=='1'">
+                    <div>
+                      自动创建公网CLB（<span class="text-warning">0.02元/小时</span>）以提供Internet访问入口，支持TCP/UDP协议，如web前台类服务可以选择公网访问。
+                    </div>
+                    <div>
+                      如您需要公网通过HTTP/HTTPS协议或根据URL转发，您可以在Ingress页面使用Ingress进行路由转发，<a href="">查看详情</a><i class="el-icon-edit-outline"></i>
+                    </div>
+                  </div>
+                  <div v-if="wl.radio=='2'">
+                    <div>将提供一个可以被集群内其他服务或容器访问的入口，支持TCP/UDP协议，数据库类服务如Mysql可以选择集群内访问,来保证服务网络隔离性。</div>
+                    <div>
+                      <el-checkbox v-model="wl.handlessChecked">Headless&nbsp;Service</el-checkbox>
+                      <el-tooltip content="不创建用于集群内访问的ClusterIP,访问Service名称时返回后端Pods IP地址,用于适配自有的服务发现机制。" placement="top" effect="light">
+                      <i class="el-icon-question" style="margin-left:5px;"></i>
+                      </el-tooltip>
+                      （Headless&nbsp;Service只支持创建时选择，<span class="text-error">创建完成后不支持变更访问方式）</span>
+                    </div>
+                  </div>
+                  <div v-if="wl.radio=='3'">
+                    <div>将提供一个可以被集群所在VPC下的其他资源访问的入口，支持TCP/UDP协议，需要被同一VPC下其他集群、云服务器等访问的服务可以选择VPC内网访问的形式。</div>
+                    <div>支持Ingress<a href="">查看详情</a><i class="el-icon-edit-outline"></i></div>
+                  </div>
+                  <div v-if="wl.radio=='4'">
+                    <div>提供一个主机端口映射到容器的访问方式，支持TCP&UDP， 可用于业务定制上层LB转发到Node。</div>
+                    <div>支持Ingress<a href="">查看详情</a><i class="el-icon-edit-outline"></i></div>
+                  </div>
+                </div>
+              </el-form-item>
+              <el-form-item v-show="wl.radio === '3'">
+                <el-select ></el-select> 
+                <el-select ></el-select> 
+                <span>共253个子网IP，剩247个可用</span>
+              </el-form-item>
+              <el-form-item label="负载均衡器" v-show="wl.radio=='1' || wl.radio=='3'">
+                <div class="radio1">
+                <el-radio-group   v-model='wl.loadBalance' style="margin-bottom: 5px;">
+                  <el-radio-button label="1">自动创建</el-radio-button>
+                  <el-radio-button label="2">使用已有</el-radio-button>
+                </el-radio-group>
+                </div>
+                <p v-show="wl.loadBalance=='1'">自动创建CLB用于公网/内网访问Service，请勿手动修改由TKE创建的CLB监听器，<a href="javascript:;">查看更多说明</a></p>
+                <div v-show="wl.loadBalance=='2'">使用已有的CLB用于公网/内网访问Service，不覆盖已有监听器规则，请勿手动修改由TKE创建的CLB监听器，仅支持未被容器服务TKE使用的CLB<a href="javascript:;">查看更多说明</a>
+                </div>
+              </el-form-item>
+              <el-form-item v-show="wl.loadBalance=='2'">
+                <el-select >
+
+                </el-select>  
+              </el-form-item>
+              <el-form-item label="端口映射" v-show="wl.radio !=='4'">
+                <div class="port">
+                  <!-- 头部 -->
+                  <div class="flex">
+                    <div style="width:140px;padding-left:14px">协议
+                      <el-tooltip content="使用公网/内网负载均衡时，TCP和UDP协议不能混合使用" placement="top" effect="light">
+                      <i class="el-icon-warning"></i>
+                      </el-tooltip>
+                    </div>
+                    <div style="width:250px;">容器端口
+                      <el-tooltip content="端口范围1~65535" placement="top" effect="light">
+                      <i class="el-icon-warning"></i>
+                      </el-tooltip>
+                    </div>
+                    <div>服务端口
+                      <el-tooltip content="集群外通过负载均衡域名或IP+服务端口访问服务，集群内通过服务名+服务端口访问服务" placement="top" effect="light">
+                      <i class="el-icon-warning"></i>
+                      </el-tooltip>
+                    </div>
+                  </div>
+                  <!-- 内容 -->
+                  <div style="border-top:1px solid #ddd;padding: 10px;">
+                    <div style="padding:5px 0;" v-for="(it,i) in wl.list" :key="i">
+                      <el-select class="w100" v-model="wl.portValue" placeholder="请选择">
+                        <el-option
+                          v-for="item in wl.options"
+                          :key="item.value"
+                          :label="item.label"
+                          :value="item.value">
+                        </el-option>
+                      </el-select>
+                      <el-input class="w250" style="padding-left:30px;" v-model="wl.conPort" placeholder="容器内应用程序监听的端口"></el-input>
+                      <el-input class="w250" style="padding-left:30px;" v-model="wl.servicePort" placeholder="建议与容器端口一致"></el-input>
+                      <el-tooltip class="item" effect="dark" content="不能删除，在至少指定一个设置端口" placement="right" v-if="i === 0">
+                        <i style="font-size:18px;padding-left:20px;" class="el-icon-close"></i>
+                      </el-tooltip>
+                      <i class="el-icon-close"  style="font-size:20px;margin-left:20px;cursor:pointer" v-else @click="removeprot(it)"></i>
+                    </div>					
+                  </div>
+                </div>
+                <a href="javascript:;" @click="addport()">添加端口映射</a>
+              </el-form-item>
+              <!-- 高级设置 -->
+              <div v-if="wl.isShowAdvancedSet">
+                <h3 style="padding-bottom:10px;">高级设置（选填）</h3>
+                <el-form-item label-width="150px" label="ExtermalTrafficPolicy">
+                  <el-radio v-model="wl.ETP" label="1">Cluster</el-radio>
+                  <el-radio v-model="wl.ETP" label="2">Local</el-radio>
+                  <div v-if="wl.ETP=='1'">默认均衡转发到工作负载的所有Pod</div>
+                  <div v-if="wl.ETP=='2'">能够保留来源IP，并可以保证公网、VPC内网访问（LoadBalancer）和主机端口访问（NodePort）模式下流量仅在本节点转发。Local转发使部分没有业务Pod存在的节点健康检查失败，可能存在流量不均衡的转发的风险。</div>
+                </el-form-item>
+                <div>
+                  <el-form-item label-width="150px" label="Session Affinity">
+                    <el-radio v-model="wl.SA" label="1">ClienIP</el-radio>
+                    <el-radio v-model="wl.SA" label="2">None</el-radio>
+                  </el-form-item>
+                  <div v-if="wl.SA=='1'">
+                    <el-form-item label-width="150px" label="最大会话保持时间">
+                      <el-input v-model="wl.time" class="w200"></el-input>
+                      <div>会话保持时间范围为0~86400</div>
+                    </el-form-item>
+                  </div>
+                </div>
+              </div>
+              <div v-if="!wl.isShowAdvancedSet"><a href="javascript:;" @click="showAdvancedSet()">显示高级设置</a></div>
+              <div v-if="wl.isShowAdvancedSet"><a href="javascript:;" @click="showAdvancedSet()">隐藏高级设置</a></div>
+            </div>
+          </div>
         </el-form>
         <!-- 设置主机路径 -->
         <el-dialog title="设置主机路径" :visible.sync="dialogVisiblePath" width="30%">
@@ -782,11 +922,13 @@
         nameSpaceList: [],//命名空间列表
         clusterId: '',//集群id
         loadShow: false,//是否显示加载
+        showServiceModal: true,//是否显示service
+        showMountPoint: false,//是否显示添加挂载点
         wl: {
           name: "",
           desc: "",
           type: "",
-          labels:[{key:'k8s-app',value:''}],
+          labels:[{key:'k8s-app',value: name}],
           nameSpace:'',
           dataJuan: [],
           container:[
@@ -794,11 +936,11 @@
             name:'',
             mirrorImg:'',
             versions:'',
-            mirrorPullTactics: 'Always', //镜像拉取策略
-            requestCpu:'',
-            limitCpu:'',
-            requestMemory:'',
-            limitMemory:'',
+            mirrorPullTactics: '', //镜像拉取策略
+            requestCpu:'0.25',
+            limitCpu:'0.5',
+            requestMemory:'256',
+            limitMemory:'1024',
             gpuNum: 0,
             environmentVar:[],
             citeCs:[],
@@ -809,26 +951,52 @@
           updateWay: '滚动更新（推荐）',
           updateTactics: '1',
           nodeTactics: "1",
-          replicas: 0
+          replicas: 0,
+          serviceEnbel: true,//是否启用service
+          radio: '1',
+          handlessChecked: false,
+          loadBalance: "1",
+          list:[],
+          portValue: 'TCP',
+          options: [
+            {
+              value: 'TCP',
+              label: 'TCP'
+            },
+            {
+              value: 'UDP',
+              label: 'UDP'
+            }
+          ],
+          conPort: '',
+          servicePort: '',
+          isShowAdvancedSet: true,
+          ETP: '1',
+          SA: '2',
+          time: 30,
+          pointList:[],
+          mountPath: '',
+          subPath: '',
+          pointName: '',
         },
         nameSpaceOptions:[{value:'tfy-pub',label:'tfy-pub'},{value:'default',label:'default'},{value:'kube-public',label:'kube-public'},{value:'kube-system',label:'kube-system'},],
         
         addVarFlag:true,
         	svc:{
-				show: true,
-				time: 30,
-				checked:false,
-        name: '',
-				value: 'default',
-				options: ['default','kube-public','kube-system','tfy-pub'],
-				radio: '1',
-				ETP: '1',
-				SA: '2',
-				input: '',
-				list: [{}],
-				workload: [],
-				tabPosition: 'dep'
-			},
+            show: true,
+            time: 30,
+            checked:false,
+            name: '',
+            value: 'default',
+            options: ['default','kube-public','kube-system','tfy-pub'],
+            radio: '1',
+            ETP: '1',
+            SA: '2',
+            input: '',
+            list: [{}],
+            workload: [],
+            tabPosition: 'dep'
+          },
         // 实例数量自动调节下拉框内容
         touchTactics: [{
           touch1: 'CPU',
@@ -1128,14 +1296,55 @@
         this.wl.labels.push({key:'',value:''});
         console.log(this.wl.labels);
       },
+      //填写名称后将名称默认复制给标签第一条
+      addLabel(e){
+        let labels = this.wl.labels;
+        if(labels.length > 0) {
+          for(let i = 0; i < labels.length; i++) {
+            if(labels[i].key === 'k8s-app') {
+              labels[i].value = this.wl.name;
+              return;
+            }
+          }
+        }
+      },
       delAddVar(index){
         this.wl.labels.splice(index,1)
       },
+      //选择是否启用service
+      changEnbel() {
+        if(this.serviceEnbel) {
+          this.showServiceModal = true;
+        } else {
+          this.showServiceModal = false;
+        }
+      },
+      // 删除端口
+		removeprot(item){
+			var index = this.wl.list.indexOf(item)
+			if(index !== -1){
+				this.wl.list.splice(index,1)
+			}
+		},
+		// 新增端口
+		addport(){
+			this.wl.list.push({
+				value: '',
+				key: Date.now()
+			})
+    },
+    //是否显示高级设置
+    showAdvancedSet() {
+      this.wl.isShowAdvancedSet = !this.wl.isShowAdvancedSet;
+    },
+    //
+    changIsShowmount() {
+      this.showMountPoint = true;
+    },
       //提交新增
       async submit() {
         this.loadShow = true;
-        let requestBody = '';
-        let labels = 'qcloud-app:' + this.wl.name + ",";
+        let labels = 'qcloud-app:' + this.wl.labels[0].value + ",";
         if(this.wl.labels.length > 0) {
           for(var i = 0; i < this.wl.labels.length; i++) {
             labels += this.wl.labels[i].key + ":" + this.wl.labels[i].value + ",";
@@ -1150,6 +1359,7 @@
             containerObj = {
               name: container[i].name,
               image: container[i].mirrorImg + ":" + container[i].versions,//'tpeccr.ccs.tencentyun.com/22333/sdf:tagv1',
+              imagePullPolicy: container[i].mirrorPullTactics,
               volumeMounts:[],
               resources: {
                 limits: 
@@ -1160,10 +1370,11 @@
                 requests: 
                 {
                   cpu: container[i].requestCpu,
-                  memory: container[i].requestMemory
+                  memory: container[i].requestMemory+'Mi'
                 }
               },
-              env:[],
+              env:[{name:"resource-huanjing","value":"resourcehuajingvalue"},{"name":"resourcebieming",
+"valueFrom":{"secretKeyRef":{"key":".dockercfg","name":"qcloudregistrykey","optional":false}}}],
               workingDir: "",
               command:[],
               args:[],
@@ -1174,7 +1385,7 @@
         }
         console.log("labels",labels);
         console.log("containerList",containerList);
-        requestBody = {
+        let requestBody = {
           kind: "Deployment",
           apiVersion: "apps/v1beta2",
           metadata:{
@@ -1191,7 +1402,8 @@
                 spec: {
                   volumes: [],
                   containers: containerList,
-                  restartPolicy: 'restartPolicy'
+                  restartPolicy: this.wl.mirrorPullTactics,
+                  imagePullSecrets: [{name: 'qcloudregistrykey'},{name:'tencenthubkey'}]
                 }
               },
               selector: {matchLabels: {labels}},
@@ -1202,12 +1414,37 @@
               }
             }
           }
-        }  
+        }
+        let requestBody1 = {
+          kind: "Service",
+          apiVersion: "v1",
+          metadata:{
+            name: this.wl.name,
+            namespace: this.spaceName
+            ,annotations: {"service.kubernetes.io/service.extensiveParameters": {AddressIPVersion:"IPV4"}}
+          },
+          spec:{
+            type: "LoadBalancer",
+            ports: [{name:this.wl.conPort+'-'+this.wl.servicePort+'-tcp',port:this.wl.conPort,targetPort:this.wl.servicePort,protocol:this.wl.portValue}],
+            selector:{labels},
+            externalTrafficPolicy:this.wl.ETP,
+            sessionAffinity: this.wl.SA,
+            sessionAffinityConfig:{}
+          }
+        }
+        if(this.wl.SA === 'ClientIP') {
+          requestBody1.spec.sessionAffinityConfig = {
+            clientIP: {
+              timeoutSeconds: this.wl.time
+            }
+          }
+        }
+        console.log("requestBody1",requestBody1);
         let params = {
           Method: "POST",
           Path: "/apis/platform.tke/v1/clusters/"+this.clusterId+"/apply?notUpdate=true",
           Version: "2018-05-25",
-          RequestBody: JSON.stringify(requestBody),
+          RequestBody: JSON.stringify(requestBody1)+JSON.stringify(requestBody),
           ClusterName: this.clusterId
         }
 
@@ -1475,6 +1712,27 @@ a{
 
   .bottom10 {
     margin-bottom: 10px;
+  }
+
+  .card {
+    padding: 10px;border-bottom:1px solid #dcdfe6;
+  }
+  .text-error {
+    color: #e54545;
+  }
+  .text-warning {
+  color: #ff9d00
+  }
+  .ms {
+    width: 330px;
+    padding: 5px 8px 8px 5px;
+    height: 100px;
+    border-radius: 4px;
+    border: 1px solid #dcdfe6;
+    resize: none;
+  }
+  .w250 {
+    width:250px;
   }
 
 </style>
