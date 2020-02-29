770da90d38e0fa006c16f2a59ab9a00369788ae9
zhaobobin <254787741@qq.com>
2020-03-01 02:53:34 +0800

    云监控---负载均衡

--- src/views/CM/CLBload/tab/Monitor.vue
+++ src/views/CM/CLBload/tab/Monitor.vue
@@ -1,40 +1,36 @@
 <template>
-  <div class='Monitor'>
+  <div class="Monitor">
+    <TimeDropDown :TimeArr='TimeArr' :Datecontrol='true' :Graincontrol='true' :Difference="'H'"
+      v-on:switchData="GetDat" />
+    <div class="box-dis">
+      <p>
+        <i class="el-icon-info"></i>注釋：Max、Min和Avg數值統計為當前折線圖內所有點的最大值、最小值和平均值
+      </p>
 
-    <!-- 时间粒度搜素 -->
-    <XTimeX v-on:switchData="GetDat" :classsvalue='value'></XTimeX>
-    <div class='box-dis p-style'>
-      <p><i class="el-icon-info"></i>{{ $t('CVM.clBload.zs') }}</p>
-      <!-- <p>
-        <el-button type="text">{{ $t('CVM.clBload.dcsj') }}</el-button>
-      </p> -->
     </div>
-    <div class='box-table'>
+    <div class="box-table">
       <!-- 表格 -->
-      <el-table :data="tableData" style="width: 100%" :empty-text="$t('CVM.clBload.zwsj')">
-        <el-table-column prop="" width="130">
+      <el-table :data="tableData" style="width: 100%" v-loading='TableLoad'>
+        <el-table-column prop width="200">
           <template slot-scope="scope">
-            <span style="font-size:12px;font-weight:bolder;color:#333;font-weight:600;">
-              {{scope.row.MetricName | UpName(value)}}
-              <span class="symbol">{{scope.row.symbol}}</span>
-              <el-popover placement="bottom-start" title="" width="200" trigger="hover">
-                <p>{{scope.row.MetricName | UpTitle(value)}}</p>
+            <p>
+              <span class='span_1'>{{disName[scope.row.MetricName]}}</span>
+              <span class='span_2'>{{Company[scope.row.MetricName]}}</span>
+              <el-popover placement="bottom-start" title width="200" trigger="hover">
+                <p>{{Tips[scope.row.MetricName]}}</p>
                 <i class="el-icon-warning" slot="reference"></i>
               </el-popover>
-            </span>
-
+            </p>
           </template>
         </el-table-column>
 
-        <el-table-column prop="DataPoints" width="550">
+        <el-table-column width="550">
           <template slot-scope="scope">
-            <p v-if="scope.row.DataPoints[0].Values.length==0"> {{ $t('CVM.clBload.zwsj') }}</p>
-            <div class='echart' v-if="scope.row.DataPoints[0].Values.length!=0">
-              <echart-line id="diskEchearrts-line" :time='scope.row.DataPoints[0].Timestamps | UpTime'
-                :opData='scope.row.DataPoints[0].Values' :scale=3 :period=period :xdata=false>
-              </echart-line>
+            <p v-if="scope.row.DataPoints[0].Values.length==0">暂无数据</p>
+            <div v-if="scope.row.DataPoints[0].Values.length!=0">
+              <echart-line id="diskEchearrts-line" :time="scope.row.DataPoints[0].Timestamps | UpTime"
+                :opData="scope.row.DataPoints[0].Values" :scale="3" :period="Period" :xdata="false"></echart-line>
             </div>
-
           </template>
         </el-table-column>
 
@@ -42,7 +38,8 @@
           <template slot-scope="scope">
             <p style="font-size:12px;color:#bbb;font-weight:600">Max:</p>
             <template v-if="scope.row.DataPoints[0].Values.length!==0">
-              <span style="color:#333;font-weight:600;font-size: 12px;">{{scope.row.DataPoints[0].Values|CMMax}}</span>
+              <span style="color:#333;font-weight:600;font-size: 12px;">{{scope.row.DataPoints[0].Values|CMMax}} <span
+                  class='span_2'>{{Company[scope.row.MetricName]}}</span></span>
               <span style="color:#333;font-weight:600;font-size: 12px;">{{scope.row.symbol}}</span>
             </template>
             <template v-if="scope.row.DataPoints[0].Values.length==0">-</template>
@@ -52,7 +49,8 @@
           <template slot-scope="scope">
             <p style="font-size:12px;color:#bbb;font-weight:600">Min:</p>
             <template v-if="scope.row.DataPoints[0].Values.length!==0">
-              <span style="color:#333;font-weight:600;font-size: 12px;">{{scope.row.DataPoints[0].Values|CMMin}}</span>
+              <span style="color:#333;font-weight:600;font-size: 12px;">{{scope.row.DataPoints[0].Values|CMMin}} <span
+                  class='span_2'>{{Company[scope.row.MetricName]}}</span></span>
               <span style="color:#333;font-weight:600;font-size: 12px;">{{scope.row.symbol}}</span>
             </template>
             <template v-if="scope.row.DataPoints[0].Values.length==0">-</template>
@@ -63,115 +61,171 @@
           <template slot-scope="scope">
             <p style="font-size:12px;color:#bbb;font-weight:600">Avg:</p>
             <template v-if="scope.row.DataPoints[0].Values.length!==0">
-              <span style="color:#333;font-weight:600;font-size: 12px;">{{scope.row.DataPoints[0].Values|CMAvg}}</span>
+              <span style="color:#333;font-weight:600;font-size: 12px;">{{scope.row.DataPoints[0].Values|CMAvg}} <span
+                  class='span_2'>{{Company[scope.row.MetricName]}}</span></span>
               <span style="color:#333;font-weight:600;font-size: 12px;">{{scope.row.symbol}}</span>
             </template>
             <template v-if="scope.row.DataPoints[0].Values.length==0">-</template>
           </template>
         </el-table-column>
-
-
-
-        <el-table-column prop="">
-          <template slot-scope="scope">
-            <!-- <p> <i class="el-icon-menu i-font" style="font-size:26px;" @click="Modality(scope.row.MetricName)"></i>
-            </p> -->
-
-          </template>
-        </el-table-column>
-
       </el-table>
-      <!-- 模态框 -->
-      <el-dialog :title="$t('CVM.clBload.jqjkzt')" :visible.sync="dialogVisible" width="60%"
-        :before-close="handleClose">
-        <XTimeX v-on:switchData="GetDat" :classsvalue='value'></XTimeX>
-        <echart-line id="diskEchearrts-line" class="echart-wh" :time='timeData | UpTime' :opData='jingData'
-          :period=period :xdata=true>
-        </echart-line>
-      </el-dialog>
 
     </div>
-
   </div>
 </template>
-
 <script>
-  import moment from 'moment';
-  import XTimeX from '@/components/public/TimeXK';
-  import echartLine from '@/components/public/echars-line';
-  import {
-    All_MONITOR
-  } from '@/constants';
+  import moment from "moment";
+  import TimeDropDown from '@/components/public/TimeDropDown' //引入时间组件
+  import echartLine from "@/components/public/echars-line"; //引入图标组件
   import {
     ErrorTips
-  } from '@/components/ErrorTips'
+  } from "@/components/ErrorTips";
+  import {
+    ALL_Basics,
+    All_MONITOR
+  } from '@/constants'
   export default {
     data() {
       return {
+        TimeArr: [{
+            name: '实时',
+            Time: 'realTime',
+            TimeGranularity: [{
+                value: "60",
+                label: "1分鐘"
+              },
+              {
+                value: "300",
+                label: "5分鐘"
+              }
+            ]
+          },
+          {
+            name: '近24小时',
+            Time: 'Nearly_24_hours',
+            TimeGranularity: [{
+                value: "60",
+                label: "1分鐘"
+              },
+              {
+                value: "300",
+                label: "5分鐘"
+              },
+              {
+                value: "3600",
+                label: "1小時"
+              },
+              {
+                value: "86400",
+                label: "1天"
+              }
+            ]
+          },
+          {
+            name: '近7天',
+            Time: 'Nearly_7_days',
+            TimeGranularity: [{
+                value: "3600",
+                label: "1小時"
+              },
+              {
+                value: "86400",
+                label: "1天"
+              }
+            ]
+          }
+        ],
+        Status: this.$route.query.Status,
         vip: this.$route.query.vip,
-        period: '',
-        Start_End: [],
-        value: 1,
-        dialogVisible: false, // 模态框 （放大后的折线图）
-        pageIndex: 1, // 当前页
-        pageSize: 10, // 每页数
-        totalPage: 0, // 表格数据数组长度
-        tableData: [], // 获取列表数据
-        timeData: [], // 折线图的x轴数据
-        jingData: [],
-        MetricName: ''
-      };
+        VpcId: this.$route.query.VpcId,
+        BaseList: [], //全部指标列表
+        BaseListK: [], //用到的指标列表
+        TableLoad: true,
+        Period: '', //粒度
+        Time: {}, //监控传递时间
+        MonitorData: [], //监控数据
+        tableData: [], // 组合数据
+        disName: {
+          'ConNum': '当前连接数',
+          'InPkg': '入包量',
+          'InTraffic': '公网入带宽',
+          'NewConn': '新增连接数',
+          'OutPkg': '公网出包量',
+          'OutTraffic': '公网出带宽'
+        },
+        Company: {
+          'ConNum': '个',
+          'InPkg': ' 个/s',
+          'InTraffic': ' Mbps',
+          'NewConn': '个',
+          'OutPkg': ' 个/s',
+          'OutTraffic': ' Mbps'
+        },
+        Tips: {
+          'ConNum': '公网连接数',
+          'InPkg': '入包量',
+          'InTraffic': '公网入带宽',
+          'NewConn': '新增连接数',
+          'OutPkg': '公网出包量',
+          'OutTraffic': '公网出带宽'
+        },
+
+      }
     },
     components: {
-      echartLine,
-      XTimeX,
+      TimeDropDown,
+      echartLine
     },
-    created() {
-
+    watch: {
+      MonitorData(val) {
+        if (this.MonitorData) {
+          this.MonitorData.forEach(element => {
+            this.BaseListK.forEach(item => {
+              if (item.MetricName === element.MetricName) {
+                item.DataPoints = element.DataPoints
+              }
+            });
+          });
+          if (this.BaseListK.length == val.length) {
+            this.tableData = this.BaseListK
+            this.TableLoad = false
+          }
+        }
+      }
     },
     methods: {
-      //获取数据
       GetDat(data) {
-        this.period = data[0];
-        this.Start_End = data[1];
-        this.value = data[2]
-        const metricNArr = [
-          'Connum',
-          'NewConn',
-          'Intraffic',
-          'Outtraffic',
-          'Inpkg',
-          'Outpkg'
-        ];
-        const symbol = ["個/分鐘", "個/分鐘", "Mbps", "Mbps", "個/秒", "個/秒"];
-        this.tableData = []
-        for (let i = 0; i < metricNArr.length; i++) {
-          this.Obtain(metricNArr[i], symbol[i]);
-        }
-        if (this.MetricName) {
-          this.getModality(this.MetricName)
-        }
+        this.Period = data[0]
+        this.Time = data[1]
+        this.TableLoad = true
+        this._GetBase()
       },
-      // 获取监控信息
-      Obtain(metricN, symbol) {
-        const param = {
+      //获取基础指标详情
+      _GetBase() {
+        if (this.Status === 'OPEN') {
+          var spaceName = 'QCE/LB_PUBLIC'
+        } else if (this.Status === 'INTERNAL') {
+          spaceName = 'QCE/LB_RS_PRIVATE'
+        }
+        let parms = {
           Version: '2018-07-24',
           Region: localStorage.getItem('regionv2'),
-          Namespace: 'QCE/LB_PUBLIC',
-          MetricName: metricN,
-          'Instances.0.Dimensions.0.Name': 'vip',
-          'Instances.0.Dimensions.0.Value': this.vip,
-          Period: this.period,
-          StartTime: this.Start_End.StartTIme,
-          EndTime: this.Start_End.EndTIme,
-        };
-        this.axios.post(All_MONITOR, param).then((data) => {
-          if (data.Response.Error == undefined) {
-            data.Response.symbol = symbol;
-            this.tableData.push(data.Response);
+          Namespace: spaceName
+        }
+        this.axios.post(ALL_Basics, parms).then(res => {
+          if (res.Response.Error == undefined) {
+            this.BaseList = res.Response.MetricSet
+            this.MonitorData = []
+            this.BaseListK = []
+            this.BaseList.forEach(item => {
+              if (item.Period.indexOf(Number(this.Period)) !== -1) {
+                this.BaseListK.push(item)
+                this._GetMonitorData(item.MetricName)
+              }
+            });
           } else {
             this.$message({
-              message: ErrorTips[data.Response.Error.Code],
+              message: ErrorTips[res.Response.Error.Code],
               type: "error",
               showClose: true,
               duration: 0
@@ -179,23 +233,31 @@
           }
         });
       },
-      //获取监控信息
-      getModality(MetricName) {
-        const param = {
+      //获取监控数据
+      _GetMonitorData(MetricName) {
+        if (this.Status === 'OPEN') {
+          var spaceName = 'QCE/LB_PUBLIC'
+        } else if (this.Status === 'INTERNAL') {
+          spaceName = 'QCE/LB_RS_PRIVATE'
+        }
+        let parms = {
           Version: '2018-07-24',
           Region: localStorage.getItem('regionv2'),
-          Namespace: 'QCE/LB_PUBLIC',
+          Namespace: spaceName,
+          Period: this.Period,
+          StartTime: this.Time.StartTIme,
+          EndTime: this.Time.EndTIme,
           MetricName: MetricName,
           'Instances.0.Dimensions.0.Name': 'vip',
           'Instances.0.Dimensions.0.Value': this.vip,
-          Period: this.period,
-          StartTime: this.Start_End.StartTIme,
-          EndTime: this.Start_End.EndTIme,
-        };
-        this.axios.post(All_MONITOR, param).then((data) => {
+        }
+        if (this.Status === 'INTERNAL') {
+          parms['Instances.0.Dimensions.1.Name'] = 'vpcId',
+            parms['Instances.0.Dimensions.1.Value'] = this.VpcId
+        }
+        this.axios.post(All_MONITOR, parms).then(data => {
           if (data.Response.Error == undefined) {
-            this.timeData = data.Response.DataPoints[0].Timestamps
-            this.jingData = data.Response.DataPoints[0].Values
+            this.MonitorData.push(data.Response);
           } else {
             this.$message({
               message: ErrorTips[data.Response.Error.Code],
@@ -206,88 +268,21 @@
           }
         });
       },
-      // 模态框
-      Modality(MetricName) {
-        this.MetricName = MetricName
-        this.dialogVisible = true;
-        this.getModality(this.MetricName)
-      },
-      handleClose(done) {
-        done();
-      },
-
     },
     filters: {
-      //文字过滤
-      UpName(value) {
-        if (value === 'Connum') {
-          return (value = '當前連接數');
-        }
-        if (value === 'NewConn') {
-          return (value = '新增連接數');
-        }
-        if (value === 'Intraffic') {
-          return (value = '入流量');
-        }
-
-        if (value === 'Outtraffic') {
-          return (value = ' 出流量');
-        }
-        if (value === 'Inpkg') {
-          return (value = '入包量');
-        }
-        if (value === 'Outpkg') {
-          return (value = '出包量');
-        }
-        if (value === '') {
-          return (value = '');
-        }
-      },
-      UpTitle(value) {
-        if (value === 'Connum') {
-          return (value = '當前連接數');
-        }
-        if (value === 'NewConn') {
-          return (value = '新增連接數');
-        }
-        if (value === 'Intraffic') {
-          return (value = '入流量');
-        }
-
-        if (value === 'Outtraffic') {
-          return (value = ' 出流量');
-        }
-        if (value === 'Inpkg') {
-          return (value = '入包量');
-        }
-        if (value === 'Outpkg') {
-          return (value = '出包量');
-        }
-        if (value === '') {
-          return (value = '');
-        }
-      },
       UpTime(value) {
-        let timeArr = []
+        let timeArr = [];
         for (let i = 0; i < value.length; i++) {
-          let uptime = moment(value[i] * 1000).format(
-            'YYYY-MM-DD HH:mm:ss',
-          );
-          timeArr.push(uptime)
+          let uptime = moment(value[i] * 1000).format("YYYY-MM-DD HH:mm:ss");
+          timeArr.push(uptime);
         }
-
-        return timeArr
+        return timeArr;
       }
-    },
-  };
-
-</script>
-
-<style scoped lang="scss">
-  .symbol {
-    color: #bbb;
+    }
   }
 
+</script>
+<style lang="scss" scoped>
   .Monitor {
     background: #ffffff;
     margin-top: 20px;
@@ -296,106 +291,26 @@
     -webkit-box-shadow: 0px 3px 3px #c8c8c8;
     -moz-box-shadow: 0px 3px 3px #c8c8c8;
     box-shadow: 0px 3px 3px #c8c8c8;
-  }
+    padding: 20px;
 
-  .box-dis {
-    display: flex;
-    justify-content: space-between;
-    margin-top: 40px;
-
-    .btn-style {
-      margin-left: 20px;
-      display: flex;
-      line-height: 32px;
-
-      .drop {
-        margin-left: 30px;
-
-        span {
-          color: #cccccc;
-          font-size: 10px;
-        }
-
-        ::v-deep.el-input {
-          width: 100px !important;
-          border: none;
-        }
-      }
-    }
-
-    p:nth-child(1) {
+    .box-dis {
+      margin-top: 20px;
       color: #ccc;
       font-size: 14px;
-      margin-left: 30px;
     }
 
-    p:nth-child(2) {
-      margin-right: 20px;
-    }
-  }
-
-  .btn-sty {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  .p-dis {
-    display: flex;
-
-    span {
-      line-height: 40px;
-    }
-
-    .width-date {
-      width: 150px;
-      margin-left: 20px;
-    }
-  }
-
-  .margin-row {
-    margin-top: 30px;
-    margin-left: 55%;
-  }
-
-  .dateheight {
-    height: 34px;
-  }
-
-  ::v-deep.echart-wh {
-    width: 100% !important;
-    height: 500px !important;
-  }
-
-  .btn-style {
-    margin-left: 20px;
-    display: flex;
-    line-height: 32px;
-
-    .drop {
-      margin-left: 500px;
-
-      span {
-        color: #cccccc;
-        font-size: 10px;
+    .box-table {
+      .span_1 {
+        font-size: 14px;
+        font-weight: bold;
+        color: black;
       }
 
-      ::v-deep.el-input {
-        width: 100px !important;
-        border: none;
+      .span_2 {
+        font-size: 12px;
+        color: #BBBBBB;
       }
     }
-  }
-
-  .box-table {
-    width: 100%;
-  }
-
-  ::v-deep.i-font {
-    font-size: 36px;
-  }
-
-  ::v-deep.el-button--small {
-    font-size: 14px !important;
   }
 
 </style>
