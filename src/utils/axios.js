1e86468d444b56c3822a4fcc088ebd68bf8bc01e
yhs810777103 <810777103@qq.com>
2020-03-05 11:19:50 +0800

    更新修改

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -11,8 +11,8 @@ const conf = {
   loading: true,
   withCredentials: true,
   headers: {
-    uuid: VueCookie.get('uuid'),
-    // uuid: 100011921910,
+    // uuid: VueCookie.get('uuid'),
+    uuid: 100011921910,
     Pragma: 'no-cache',
     'Cache-Control': 'no-cache',
     'Content-Type': 'application/json; charset=utf-8'
@@ -47,12 +47,12 @@ Plugin.install = function (vue) {
   window.axios = instance
   Object.defineProperties(vue.prototype, {
     axios: {
-      get() {
+      get () {
         return instance
       }
     },
     $axios: {
-      get() {
+      get () {
         return instance
       }
     }
