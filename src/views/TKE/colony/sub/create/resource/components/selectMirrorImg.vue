3617a93192cce1decc42a11b7f7071fdbd637d11
2667579369@qq.com <Zheng19950915>
2020-03-06 18:40:53 +0800

    zsj 选中镜像组件临时提交

--- src/views/TKE/colony/sub/create/resource/components/selectMirrorImg.vue
+++ src/views/TKE/colony/sub/create/resource/components/selectMirrorImg.vue
@@ -1,6 +1,6 @@
 <template>
   <el-dialog title="选择镜像" :visible.sync="visible" class="dialog" :before-close="handleClose" :close-on-click-modal='set'
-    :close-on-press-escape='set'>
+             :close-on-press-escape='set'>
     <div class="dialog-content">
       <p>
         <el-radio-group v-model="radio" text-color="#000">
@@ -13,112 +13,196 @@
         <el-tab-pane label="公有镜像" name="third"></el-tab-pane>
         <el-tab-pane label="Docker Hub镜像" name="fourth"></el-tab-pane>
       </el-tabs>
-      <div v-show="activeName=='first'||activeName=='third'">
-        <el-radio-group v-model="radio1">
-          <el-radio-button v-for="(v,i) in city" :key="i" :label="v"></el-radio-button>
-        </el-radio-group>
-        <p class="wdred">建议您选择与容器集群相同地域的镜像仓库，访问不同地域的镜像仓库受公网网络出入带宽影响。</p>
-        <el-input placeholder="请输入镜像名称" class="input-set">
-          <i slot="suffix" class="el-input__icon el-icon-search"></i>
-        </el-input>
-      </div>
-      <div v-show="activeName=='second'||activeName=='fourth'">
-        <el-input placeholder="请输入镜像名称" class="input-set">
-          <i slot="suffix" class="el-input__icon el-icon-search"></i>
-        </el-input>
-      </div>
+      <el-input placeholder="请输入镜像名称" class="input-set">
+        <i slot="suffix" class="el-input__icon el-icon-search"></i>
+      </el-input>
+      <div v-show="activeName === 'first'">
         <el-table height="300" :data="tableData.slice((currpage - 1) * pagesize, currpage * pagesize)"
-          tooltip-effect="dark" style="width: 100%">
+                  tooltip-effect="dark" style="width: 100%" highlight-current-row @current-change="tableCurrentChange">
           <el-table-column width="55">
             <template slot-scope="scope">
-              <el-radio v-model="id" :label="scope.row.id">&nbsp;</el-radio>
+              <el-radio v-model="mirrorImageRadio" :label="scope.row.reponame">&nbsp;</el-radio>
             </template>
           </el-table-column>
           <el-table-column label="名称" width="120">
-            <template slot-scope="scope">{{ scope.row.ID }}</template>
+            <template slot-scope="scope">{{ scope.row.name }}</template>
           </el-table-column>
-          <el-table-column prop="name" label="类型" width="120">
+          <el-table-column prop="publicText" label="类型" width="120">
           </el-table-column>
-          <el-table-column prop="address" label="命名空间">
+          <el-table-column prop="namespace" label="命名空间">
           </el-table-column>
           <el-table-column prop="address" label="仓库地址" show-overflow-tooltip>
+            <template slot-scope="scope">tpeccr.ccs.tencentyun.com/{{ scope.row.reponame }}</template>
           </el-table-column>
         </el-table>
-        <!-- 分页 -->
-        <div class="pagstyle" style="height:70px;">
-          <span>共&nbsp;{{ TotalCount }}&nbsp;项</span>
-          <div class="pagestyle_right">
-            <el-pagination :page-size="pagesize" :pager-count="7" :page-sizes="[50, 40, 30, 20, 10]"
-              layout="sizes, prev, pager, next" @current-change="handleCurrentChange" @size-change="handleSizeChange"
-              :total="TotalCount"></el-pagination>
-          </div>
+      </div>
+      <div v-show="activeName!=='first'">
+        <el-table :data="tableData2" height="300" tooltip-effect="dark" style="width: 100%"
+                  highlight-current-row @current-change="tableCurrentChange">
+          <el-table-column width="55">
+            <template slot-scope="scope">
+              <el-radio v-model="mirrorImageRadio" :label="scope.row.reponame">&nbsp;</el-radio>
+            </template>
+          </el-table-column>
+          <el-table-column prop="reponame" label="名称" width="220">
+          </el-table-column>
+          <el-table-column prop="repotype" label="类型" width="220">
+          </el-table-column>
+          <el-table-column prop="favorCount" label="收藏量">
+          </el-table-column>
+        </el-table>
+      </div>
+      <!-- 分页 -->
+      <div class="pagstyle" style="height:70px;">
+        <span>共&nbsp;{{ TotalCount }}&nbsp;项</span>
+        <div class="pagestyle_right">
+          <el-pagination :page-size="pagesize" :pager-count="7" :page-sizes="[50, 40, 30, 20, 10]"
+                         layout="sizes, prev, pager, next" @current-change="handleCurrentChange"
+                         @size-change="handleSizeChange"
+                         :total="TotalCount">
+          </el-pagination>
         </div>
       </div>
+    </div>
     <span slot="footer" class="dialog-footer">
       <el-button @click="cancle">取 消</el-button>
       <el-button type="primary" @click="sure">确 定</el-button>
     </span>
   </el-dialog>
 </template>
-<script>
-  export default {
-    props: ['dialogVisible'],
-    data() {
-      return {
-        visible: false,
-        set: false,
-        radio: '选择容器服务镜像',
-        radio1: '默认区域',
-        activeName: 'first',
-        city: ['默认区域', '1', '2', '3', '4', '5', '6', '7', '8'],
-        //分页
-        tableData: [],
-        TotalCount: 0,
-        pagesize: 10,
-        currpage: 1,
+<script type="text/javascript">
+import { SPACENAME_LIST, MIRROR_LIST, GETFAVOR, GET_REPOSITORY_LIST, TKE_DOCKERHUB_LIST } from '@/constants'
+export default {
+  props: ['dialogVisible'],
+  data () {
+    return {
+      visible: false,
+      set: false,
+      radio: '选择容器服务镜像',
+      // radio1: '默认区域',
+      activeName: 'first',
+      // city: ['默认区域', '1', '2', '3', '4', '5', '6', '7', '8'],
+      // 分页
+      mirrorImageRadio: '',
+      tableData: [],
+      tableData2: [],
+      TotalCount: 0,
+      pagesize: 10,
+      currpage: 1,
+    }
+  },
+  watch: {
+    dialogVisible (val) {
+      if (val) this.initRequery()
+      this.visible = val
+    },
+    visible (val) {
+      this.$emit('update:dialogVisible', val)
+    },
+    activeName (val) {
+      this.mirrorImageRadio = ''
+      let arr = {
+        first: this.searchUserRepository,
+        second: this.getFavor,
+        third: this.getRepositoryList,
+        fourth: this.getDockerHubRepositoryList
+      }
+      arr[val]()
+    }
+  },
+  methods: {
+    tableCurrentChange: function (row) {
+      if (!row) return
+      this.mirrorImageRadio = row.reponame
+      console.log(row)
+    },
+    initRequery: function () {
+      this.getNamespaceInfo()
+      this.searchUserRepository()
+    },
+    getNamespaceInfo: async function () {
+      await this.axios.post(SPACENAME_LIST, { limit: 100 }).then(res => {
+        console.log(res)
+      })
+    },
+    searchUserRepository: async function () {
+      await this.axios.post(MIRROR_LIST, {
+        offset: 0,
+        limit: 20
+      }).then(res => {
+        let { data: { repoInfo } } = res
+        repoInfo.forEach(item => {
+          let reponameArr = item.reponame.split('/')
+          item.namespace = reponameArr[0]
+          item.name = reponameArr[1]
+          item.publicText = item.public ? '公有' : '私有'
+        })
+        this.tableData = repoInfo
+        console.log('searchUserRepository', this.tableData)
+      })
+    },
+    getFavor: async function () {
+      await this.axios.post(GETFAVOR, {
+        offset: 0,
+        limit: 20
+      }).then(res => {
+        let { data: { repoInfo } } = res
+        this.tableData2 = repoInfo
+      })
+    },
+    getRepositoryList: async function () {
+      await this.axios.post(GET_REPOSITORY_LIST, {
+        offset: 0,
+        limit: 20
+      }).then(res => {
+        let { data: { repoInfo } } = res
+        this.tableData2 = repoInfo
+      })
+    },
+    getDockerHubRepositoryList: async function () {
+      await this.axios.post(TKE_DOCKERHUB_LIST, {
+        offset: 0,
+        limit: 20
+      }).then(res => {
+        let { data: { repoInfo } } = res
+        this.tableData2 = repoInfo
+      })
+    },
+    handleClose (done) {
+      this.visible = false
+      this.$emit('close', this.visible)
+    },
+    cancle () {
+      this.visible = false
+      this.$emit('close', this.visible)
+    },
+    sure () {
+      this.visible = false
+      if (this.activeName === 'first') {
+        if (this.mirrorImageRadio !== '') {
+          this.$emit('confirm', `tpeccr.ccs.tencentyun.com/${this.mirrorImageRadio}`)
+        }
+      } else {
+        this.$emit('confirm', this.mirrorImageRadio)
       }
     },
-    watch: {
-      dialogVisible(val) {
-        console.log(val)
-        this.visible = val
-      }
+    handleClick (tab) {
+      this.activeName = tab
     },
-    mounted() {
-      console.log(this.dialogVisible)
+    // 改变页数
+    handleCurrentChange (val) {
+      this.currpage = val
     },
-    methods: {
-      handleClose(done) {
-        this.visible = false;
-        this.$emit('close', this.visible)
-
-      },
-      cancle() {
-        this.visible = false;
-        this.$emit('close', this.visible)
-      },
-      sure() {
-        this.visible = false;
-        this.$emit('close', this.visible)
-      },
-      handleClick(tab) {
-        this.activeName = tab;
-      },
-      // 改变页数
-      handleCurrentChange(val) {
-        this.currpage = val;
-      },
-      handleSizeChange(val) {
-        console.log(`每頁 ${val} 條`);
-      },
-    },
+    handleSizeChange (val) {
+      console.log(`每頁 ${val} 條`)
+    }
   }
+}
 
 </script>
 <style scoped lang="scss">
   /* css */
-
-  .dialog>>>.el-dialog {
+  .dialog > > > .el-dialog {
     height: 100%;
     margin-top: 0px !important;
     position: fixed;
@@ -129,7 +213,7 @@
     overflow: auto;
   }
 
-  .dialog>>>.el-dialog__footer {
+  .dialog > > > .el-dialog__footer {
     text-align: center !important;
   }
 
@@ -138,12 +222,13 @@
   }
 
   .dialog-content {
+    height: auto;
     min-height: 500px;
     background: #f2f2f2;
     padding: 10px;
   }
 
-  .input-set>>>input {
+  .input-set > > > input {
     margin: 15px 0px;
     height: 30px;
   }
