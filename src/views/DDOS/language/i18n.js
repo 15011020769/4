9eebd9dc31c6db8baf5f53331491c915536ea698
z827712820 <827712820@qq.com>
2020-03-08 10:16:07 +0800

    pull

--- src/views/DDOS/language/i18n.js
+++ src/views/DDOS/language/i18n.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 Vue.use(VueI18n)
 var i18n = new VueI18n({
-  locale: 'jt', // 语言标识
+  locale: 'ft', // 语言标识
   messages: {
     'jt': require('@/views/DDOS/language/jt'),
     'ft': require('@/views/DDOS/language/ft')
