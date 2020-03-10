d33c03afea857da39d9b70376e1e77a98dc5340e
yhs810777103 <810777103@qq.com>
2020-03-10 23:48:04 +0800

    云监控模板

--- src/constants/CM-yhs.js
+++ src/constants/CM-yhs.js
@@ -11,13 +11,22 @@ export const COPY_TEMPLATE = 'monitor/CopyConditionsTemplate'
 export const DELETE_TEMPLATE = 'monitor/DeleteConditionsTemplate'
 
 // 获取告警策略组列表
-export const GET_GROUP_LIST = 'monitor/GetPolicyGroupList'
+export const GET_GROUP_LIST = 'monitor2/GetPolicyGroupList'
 
 // 获取传输日志
 export const GET_TRANS_LOG = 'monitor/GetTransLog'
 
 // 修改名称和备注
-export const UPDATE_INFO = 'monitor2/UpdatePolicyGroupInfo'
+export const UPDATE_INFO = 'monitor/UpdatePolicyGroupInfo'
 
-// 编辑触发条件
+// 编辑触发条件(描述基本指标)
 export const DESCRIBE_METRICS = 'monitor/DescribeBaseMetrics'
+
+// 使用预制触发条件
+// export const DESCRIBE_METRICS = 'monitor/DescribeBaseMetrics'
+
+// 新建触发条件模板
+export const UPDATE_TEMPLATE = 'monitor/UpdateConditionsTemplate'
+
+// 获取条件模板列表
+export const GET_TENCENTCLOUDAPI = 'monitor2/tencentcloudapi'
