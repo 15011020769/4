8940547b636324bab99f10e66f2596c324db1f10
Mr.H <470817974@qq.com>
2020-03-14 11:39:05 +0800

    基础防护中攻击日志下载显示异常问题修复

--- src/views/DDOS/ProtectOverview/ProtectOverview.vue
+++ src/views/DDOS/ProtectOverview/ProtectOverview.vue
@@ -229,7 +229,7 @@
                 prop="Mbps"
                 :label="$t('DDOS.Protective.AgainstBandwidth')"
               >
-                <template slot-scope="scope">{{ scope.row.Mbps }}</template>
+                <template slot-scope="scope">{{ scope.row.Mbps }}bps</template>
               </el-table-column>
               <el-table-column
                 prop="Pps"
@@ -501,7 +501,8 @@ export default {
     },
     exportExcel() {
       /* generate workbook object from table */
-      var wb = XLSX.utils.table_to_book(document.querySelector("#exportTable"));
+      var xlsxParam = { raw: true }
+      var wb = XLSX.utils.table_to_book(document.querySelector("#exportTable"),xlsxParam);
       /* get binary string as output */
       var wbout = XLSX.write(wb, {
         bookType: "xlsx",
@@ -518,7 +519,7 @@ export default {
       }
       return wbout;
     },
-     // 获取持续时间
+    // 获取持续时间
     durationDate (endTime, StartTime) {
       let durationTime = ''
       let stime = new Date(StartTime).getTime()
