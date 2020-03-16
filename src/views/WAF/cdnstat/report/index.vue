b12b54494086d866876c085c41cdb41254d88523
iSayid <sayid>
2020-03-16 10:00:57 +0800

    CDN

--- src/views/WAF/cdnstat/report/index.vue
+++ src/views/WAF/cdnstat/report/index.vue
@@ -125,7 +125,7 @@ export default {
       } else if (interval === 'day') {
         times = [time, ]
       }
-      return {interval, projectId, domainName, time}
+      return {interval, projectId, domainName, times}
     }
   },
   watch: {
