ebf8e946fbaf0c47429ec0668790003f65c9cb16
wangyuren <997959694@qq.com>
2020-03-20 14:16:08 +0800

    更新undefined

--- src/views/CM/CM_assembly/product_type.vue
+++ src/views/CM/CM_assembly/product_type.vue
@@ -165,7 +165,7 @@
           Region: localStorage.getItem("regionv2"),
           Version: "2017-03-12",
         }
-        if (this.searchParam.label !== '' && this.searchParam.value !== '') {
+        if (this.searchParam.label !== undefined && this.searchParam.value !== undefined) {
           parms["Filters.0.Name"] = this.searchParam.label;
           parms["Filters.0.Values.0"] = this.searchParam.value;
           parms["Filters.1.Name"] = 'project-id';
