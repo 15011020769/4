ffa5ca6ee0493c03540aea7edb528048d7d7c212
Austin5303 <15854750313@163.com>
2020-03-20 11:27:09 +0800

    腾讯1006537 修改了订单查询与下载不符的bug

--- src/views/BILL/Deal/Deal.vue
+++ src/views/BILL/Deal/Deal.vue
@@ -85,7 +85,6 @@
             align="center"
             width="120"
             :label="$t('BILL.Deal.status')"
-            :formatter="formatterStatus"
           ></el-table-column>
           <el-table-column
             prop="projectName"
@@ -380,7 +379,9 @@ export default {
       var params = {
         projectId: this.dataForm.projectId,
         orderId: this.dataForm.orderId,
-        orderOwner: this.$cookie.get('uin')
+        orderOwner: this.$cookie.get('uin'),
+        beginDate: this.dataForm.date[0],
+        endDate: this.dataForm.date[1]
       }
       this.downloadLoading = true
       this.axios
