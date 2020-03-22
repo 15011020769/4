b8c62cec53aab87df58d9a7aa9b110d13e147e55
dudan <842157776@qq.com>
2020-03-22 18:29:20 +0800

    cdnie11不兼容includes

--- src/views/WAF/main.js
+++ src/views/WAF/main.js
@@ -87,6 +87,35 @@ if (!Float32Array.prototype.slice) {
       return target;
   };
 }
+// 兼容ie11对象不支持includes
+if (!Array.prototype.includes) {
+  Object.defineProperty(Array.prototype, 'includes', {
+    value: function(valueToFind, fromIndex) {
+
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+      var o = Object(this);
+      var len = o.length >>> 0;
+      if (len === 0) {
+        return false;
+      }
+      var n = fromIndex | 0;
+      var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+      function sameValueZero(x, y) {
+        return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+      }
+      while (k < len) {
+        if (sameValueZero(o[k], valueToFind)) {
+          return true;
+        } 
+        k++;
+      }
+      return false;
+    }
+  });
+}
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
