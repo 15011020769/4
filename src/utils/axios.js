02938c07a85f8c025d024d5e3951914c5f609e22
zhaobobin <254787741@qq.com>
2020-03-23 07:26:34 +0800

    axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -11,8 +11,8 @@ const conf = {
   loading: true,
   withCredentials: true,
   headers: {
-    // uuid: VueCookie.get('uuid'),
-    uuid: 100013247326,
+    uuid: VueCookie.get('uuid'),
+    // uuid: 100013247326,
     Pragma: 'no-cache',
     'Cache-Control': 'no-cache',
     'Content-Type': 'application/json; charset=utf-8'
