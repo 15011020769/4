bc03cff235c4c0fcefea6ac7cea18cab434cbd39
wxy78963 <wxy4579@163.com>
2020-03-25 13:07:48 +0800

    告警策略詳情

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -12,7 +12,8 @@ const conf = {
   withCredentials: true,
   headers: {
     // uuid: VueCookie.get('uuid'),
-    uuid: 100011921910,
+    uuid: 100013247326,
+    // uuid: 100011921910,
     Pragma: 'no-cache',
     'Cache-Control': 'no-cache',
     'Content-Type': 'application/json; charset=utf-8'
--- src/views/CM/strategy/components/defaultDetail.vue
+++ src/views/CM/strategy/components/defaultDetail.vue
@@ -151,8 +151,22 @@
           </el-table-column>
           <el-table-column label="IP地址" v-if="ViewName === 'cvm_device'">
             <template slot-scope="scope">
-              <p>{{ scope.row.PrivateIpAddresses[0] }}(内网)</p>
-              <p>{{ scope.row.PublicIpAddresses[0] }}(外网)</p>
+              <p>
+                <span
+                  v-for="(item, index) in scope.row.PrivateIpAddresses"
+                  :key="index"
+                >
+                  {{ item }}(内网)
+                </span>
+              </p>
+              <p>
+                <span
+                  v-for="(item, index) in scope.row.PublicIpAddresses"
+                  :key="index"
+                >
+                  {{ item }}(外网)
+                </span>
+              </p>
             </template>
           </el-table-column>
           <!-- BS -->
@@ -501,8 +515,10 @@
                       v-if="ViewName === 'cvm_device'"
                     >
                       <template slot-scope="scope">
-                        <p>{{ scope.row.PrivateIpAddresses[0] }}(内网)</p>
-                        <p>{{ scope.row.PublicIpAddresses[0] }}(外网)</p>
+                        <p>
+                          <span v-for="(item,index) in scope.row.PrivateIpAddresses" :key="index">{{item}}(内网)</span></p>
+                        <p><span v-for="(item,index) in scope.row.PublicIpAddresses" :key="index">{{item}}(外网)</span>
+                         </p>
                       </template>
                     </el-table-column>
                     <!-- BS -->
