356bf6b2ea1afeff975ebf8e811b5510a475c97a
邓涛 <taodengzhy@163.com>
2020-04-09 23:12:27 +0800

    修改超时

--- src/utils/timeout.js
+++ src/utils/timeout.js
@@ -2,7 +2,7 @@ import { clearLoginInfo } from '@/utils'
 
 let lastTime = new Date().getTime();
 let currentTime = new Date().getTime();
-let timeOut = 3 * 60 * 1000; //设置超时时间： 60分钟
+let timeOut = 60 * 60 * 1000; //设置超时时间： 60分钟
   
 window.onload = function () {
   window.document.onmousedown = function () {
@@ -18,4 +18,4 @@ function checkTimeout() {
 }
   
 /* 定时器 间隔30秒检测是否长时间未操作页面 */
-window.setInterval(checkTimeout, 1 * 60 * 1000);   // 5分钟检查一次
+window.setInterval(checkTimeout, 5 * 60 * 1000);   // 5分钟检查一次
