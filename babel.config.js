3e030df0faf6c01ff4646e8175ac2822f1dbc15c
iSayid <sayid>
2020-04-13 17:18:10 +0800

    还原

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,4 @@
-const plugins = []
-// 生产环境移除console
-if(process.env.NODE_ENV === 'production') {
-  plugins.push("transform-remove-console")
-}
-
 module.exports = {
-  plugins: plugins,
   presets: [
     '@vue/app'
   ]
