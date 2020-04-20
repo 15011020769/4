8e92e2ddf049f79c8d3f59c4556f48beb7c898f9
邓涛 <taodengzhy@163.com>
2020-04-20 20:17:43 +0800

    scf默认模板json

---
+++ src/views/SCF/lib/template.js
@@ -0,0 +1,19 @@
+const template = [
+  {
+    type: 'Python 3.6',
+    detail: [{
+      name: 'helloworld',
+      language: 'Python 3.6',
+      desc: 'helloworld 空白模板函數',
+      label: ['Python3.6', 'helloworld'],
+      author: '台富雲無伺服器雲函数团队',
+      inParams: '{ "event": "", "context": "" }',
+      outParams: '{ "response": { "body": "res_text", "headers": { "Access-Control-Allow-Origin": "*", "Content-Type": "text" }, "isBase64Encoded": "False", "statusCode": 200 } }',
+      notice: '無',
+      certificate: '公开',
+      url: '1.zip'
+    }]
+  }
+]
+
+export default template
